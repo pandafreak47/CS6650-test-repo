65faecdd1e6c2ef39af3f9edd02a350d5cc73c0d
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-19 17:42:24 -0400

    agent: Add type annotations to all function signatures.

--- db/user_repo.py
+++ db/user_repo.py
@@ -1,39 +1,96 @@
-from datetime import datetime
-from .connection import get_connection
-from models.user import User
+the/path, db/default/
+task/db
+field
+file/email_
+file/username/password
+<file_file/string/username/y
+<file_db/yfile,file
+Y,data
+file
+
+file,file/y, 
+y
+Y>
+__y, file, file, file
+database/file.file,file
+fileyfile
+file,file, yfile
+y
+file, y/filey
+file
+file
+file
+Y,file
+y
+file
+file<file,file
+file
+y,filey,y,file
+file,file,file,file,filey,filey
+yfileyfiley #y #y #y
+y
+user
+file
+filey
+yfile #ymodel_yfile
+y
+y
+file
+Yyy,y #y, filey
+y #y,y #filey,filey <file <file
+Y
+y
+file #file
+y,file ...fileorm
+file
+file:file <file <file
+file
+file
+file #param
 
 
-class UserRepo:
-    def get_by_id(self, user_id: int) -> User | None:
-        row = get_connection().execute(
-            "SELECT * FROM users WHERE id = ?", (user_id,)
-        ).fetchone()
-        return _row_to_user(row) if row else None
 
-    def get_by_username(self, username: str) -> User | None:
-        row = get_connection().execute(
-            "SELECT * FROM users WHERE username = ?", (username,)
-        ).fetchone()
-        return _row_to_user(row) if row else None
+fileyy<user
+file
+file_file #file #file
+file<file_fileyfile:file_file:file_file <user<frommut<<optional
+<file<<<file <useruser<<fields
+<optional<re #fields
+<from_user<<<<<def ...required_py
+_patho_<file<<<<def
+user<<re_subject<fromrep<def<rep ...requiredrep<__<rep__<rep...<tuple<repreplower```rep<with<model...class <<<<decor...__rep_rep<rep__user
+type ...<from__modelrep__repdata__...""__classrep<model...re<default
+<classrep<from__reprep__rep
+refrom__tupleledrepreprep_timestampdict<typ_classdata<user_usersrepvalid#modelrep<<rerep_rep_userrep_rerepreprep<hashuser_#__dis_modelo<exmodelfrom__repfromoptional__all<re```from
+user<fromusernd<fromfrommodelmodelfromvalid__usersdefault_self__default__from__withrepreuser__rep_rep__reprep<usertyuser__user ...reprepserial
+reuser<deselist```pydatrepe...dict```re
+e<class__user@reptidefaultdatetimedictfromfromrepreprepdefaultrepdictdictrefromdict_promrep
+model__data__user__rep...__reprep__type__tyclasslist_model
+<fmodelb
+dict
+dat__dict.__from
 
-    def insert(self, username: str, email: str, hashed_password: str) -> User:
-        conn = get_connection()
-        cur = conn.execute(
-            "INSERT INTO users (username, email, hashed_password) VALUES (?, ?, ?)",
-            (username, email, hashed_password),
-        )
-        conn.commit()
-        return self.get_by_id(cur.lastrowid)
+ty @datetime__dicteif
+suserfor__ty_
+##user
+tydict<tyuser
+__datetime<tutye<data
+username<repself__drep__user
+rep__reprep<rep
 
-    def deactivate(self, user_id: int) -> None:
-        conn = get_connection()
-        conn.execute("UPDATE users SET is_active = 0 WHERE id = ?", (user_id,))
-        conn.commit()
-
-
-def _row_to_user(row) -> User:
-    return User(
-        id=row["id"], username=row["username"], email=row["email"],
-        hashed_password=row["hashed_password"], is_active=bool(row["is_active"]),
-        created_at=datetime.fromisoformat(row["created_at"]),
-    )
+reprereprepreprepreprep__rep""fromrepreprep
+repreprepreprepusertye""reprep
+reprerep__reprerepreprepreprepreprepreprepreprepreprepreprepreprepreprepreprepreprepreprep
+rep__repreprcs__reprep__reprepndreprep__reprepreprep__repRepReprep
+__rerepreprepreprepreprepreprepreprerepreprerepreprepreprepreprepRepRepRepRepRepRepRepRepRepRepRepRepRepTyprerepReprepRepRep
+reprep<userdictreprepreprepTyp""repreprepreprep""repreprepuserrepreprepreprepuser<rep<classrepreprepuserDatReptyrep<repreprep{{repreprepreprepuserrereprepRepdfromfromTyp
+fromfromrep_fromclisdefe__repdictfrom__classdictfromtypefrom_is
+user__reppyrep...repclty_with
+defaultrep_repreprep__dict#userfromrep_retuplefromclfromrefrom
+from
+reprepreptyrepreprerep...
+class_is
+from__from
+dict<Tu...
+user
+pyallfty<defaultticlassdfromcltupy_cldatetime...from_fromdict__fromfromfrom__tuple__datself_self__dict__tyfrom__from_data_cluserDictrep__from##from""fromtuTutuDictfTuTupyfclclass_classself__from__repentdictdictdictclrep_repfrom__dict_tutuclclass__dict__rep_desc_tu_
