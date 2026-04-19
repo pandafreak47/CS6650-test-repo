17dc9462453c0c29c02a59516d08ec257fcdecd9
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-19 17:22:02 -0400

    agent: Add type annotations to all function signatures.

--- api/routes.py
+++ api/routes.py
@@ -1,10 +1,34 @@
-<for__user
-<user <call <async ...userauth""""api =auth_fromauths...
-user_<user""_user =user =files_<user(defsfile =user =<user""auth_token
-users =auto
-<userasyncap""user
-touser
-user.user
-auth""<<user_auth__json_type_api
-self:s""""""""api```from""<auth""from""""""""fromapi""fromr""call_asdu""userapi""a::token""userauthor_auth""auth_error =wr<user:auth_
-...e<""<<<ap_auth_user:api_authself_
+:user""""from"""""""""" fromauth""""api""""auth""from""""auth""
+file""""
+from"""")
+from""auth"from""""auth""<user""auth""
+fromuser"""""<file""""""user""from""file""""""""""""""auth""""""""""""""""""auth""""""""""""""""""""""""<auth""user""""<auth""""""""""""""
+user""""auth""user""""""""""""""<user
+user""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""user""""""""""""/*""""`user""
+<user""""user""""<user<user""user:user`user=user""""""user""user
+<user""user
+<user""<user""user`<<user,user,file...user
+user""user`user<user""pass""<user""user
+""""""xxx andh""/*user <user/user""""""user =self and<api """user --><user=""user=""user <<user=file<pass_file.__user
+user <user.__api """user""user""userorm
+<api
+""""user""<user""<<users <async *user ...async_user<user<user =>user""user<user ->s""user""user =>auth""""""""""""""auth_for""""""""user""<api=user"" ...
+...user_auth =user =auth =...user =authauthauth""""user
+useruser""<user <<user
+```
+<appuser""<<<user
+<...""""""""""<<user:user""""<user""files""""<""""from"""""""fromapi<<async
+user:fromsas
+authoring_login
+auth""valid""auth_failing""user_fromuser ...auth""auth_for""
+""def_user""user:user""<auth""""<user_user
+auth_<<authuv<fromuserauth_login<app_fromfrom<apiauth_fromauthapi_<<<<<<<authuser""auth<<authsfromauthwrapperauth<<useraleruserauthauths<for____""_func
+<token_auth...api__<auth__api_d_auth
+<user<withauthuser_authdeautheeauthauthapiauth<authauthenticuserauth__auth_auth_auth_api_authauths_mauthorapiauthself<w_authself_s_authswrapper_auth<brsauth
+defr<<authauths_auth_auth<midauth
+authfunauthauth<auth__funfn_auth<mid_auth<authsauthauth_authauthsfn__auth__authauthsfrommidapiauth_<__authauthauthauthauthauthauthselfselfauthauth_auth""auth
+<auth<authauthauthauthauth<authauthauthauthauthauthmidauthauthauthauthauthauthauthauthauthauth<authauthauthauthauthauthauth<authauthmidauthuserauthauthauthauth
+auth<auth<auth<<<auth
+auth<authmid<auth<authauthauthauthapimidauthauthauthauthauthauthauthauth<authauth<authauthauth<b__authauthauthauthauthauth<authmid<auth_authdeffn_wrapperauth_auth<wrap_authauthauth""auth<wauth<authauth_authauthauthauthmidauth<auth<authauthauthauth<authauthauthauthauthmidauthauthapiauth<auth<auth<auth""<author<authauthauthauthauthauthauthbeauth<auth<<mid<<w<auth""authauth<<auth<auth_authwrap<authwrap_authauth<auth""mid""<beapi
+apimidwrapauthauthauthauthauth_authauth<mid_mid_use_mid_fn_wwrap_wrapperauthauthwrapper_wrapwrap_auth_wrapwrap_wrapwrapauthwrap_authuseruser_wauth.wrap_authauth_wrapper_wrap_auth<wrap_wwrap_fl_authwrapper""<mid<__<w_flwrap_wrapwrapwrapper_defauth_bemidwrapperwrapper_wrapauth<wt_wselfauthwrapper
+wwrapmidmidauthuserwrapperauth_authauthmidauthmid_username_wrap__username_midapiwrapperauthwrap_authcauth
