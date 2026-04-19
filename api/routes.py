0ad81f6836d2dc8154513e63fbcd8cd2da465774
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-19 17:25:11 -0400

    agent: Add type annotations to all function signatures.

--- api/routes.py
+++ api/routes.py
@@ -1,34 +1,9 @@
-:user""""from"""""""""" fromauth""""api""""auth""from""""auth""
-file""""
-from"""")
-from""auth"from""""auth""<user""auth""
-fromuser"""""<file""""""user""from""file""""""""""""""auth""""""""""""""""""auth""""""""""""""""""""""""<auth""user""""<auth""""""""""""""
-user""""auth""user""""""""""""""<user
-user""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""user""""""""""""/*""""`user""
-<user""""user""""<user<user""user:user`user=user""""""user""user
-<user""user
-<user""<user""user`<<user,user,file...user
-user""user`user<user""pass""<user""user
-""""""xxx andh""/*user <user/user""""""user =self and<api """user --><user=""user=""user <<user=file<pass_file.__user
-user <user.__api """user""user""userorm
-<api
-""""user""<user""<<users <async *user ...async_user<user<user =>user""user<user ->s""user""user =>auth""""""""""""""auth_for""""""""user""<api=user"" ...
-...user_auth =user =auth =...user =authauthauth""""user
-useruser""<user <<user
-```
-<appuser""<<<user
-<...""""""""""<<user:user""""<user""files""""<""""from"""""""fromapi<<async
-user:fromsas
-authoring_login
-auth""valid""auth_failing""user_fromuser ...auth""auth_for""
-""def_user""user:user""<auth""""<user_user
-auth_<<authuv<fromuserauth_login<app_fromfrom<apiauth_fromauthapi_<<<<<<<authuser""auth<<authsfromauthwrapperauth<<useraleruserauthauths<for____""_func
-<token_auth...api__<auth__api_d_auth
-<user<withauthuser_authdeautheeauthauthapiauth<authauthenticuserauth__auth_auth_auth_api_authauths_mauthorapiauthself<w_authself_s_authswrapper_auth<brsauth
-defr<<authauths_auth_auth<midauth
-authfunauthauth<auth__funfn_auth<mid_auth<authsauthauth_authauthsfn__auth__authauthsfrommidapiauth_<__authauthauthauthauthauthauthselfselfauthauth_auth""auth
-<auth<authauthauthauthauth<authauthauthauthauthauthmidauthauthauthauthauthauthauthauthauthauth<authauthauthauthauthauthauth<authauthmidauthuserauthauthauthauth
-auth<auth<auth<<<auth
-auth<authmid<auth<authauthauthauthapimidauthauthauthauthauthauthauthauth<authauth<authauthauth<b__authauthauthauthauthauth<authmid<auth_authdeffn_wrapperauth_auth<wrap_authauthauth""auth<wauth<authauth_authauthauthauthmidauth<auth<authauthauthauth<authauthauthauthauthmidauthauthapiauth<auth<auth<auth""<author<authauthauthauthauthauthauthbeauth<auth<<mid<<w<auth""authauth<<auth<auth_authwrap<authwrap_authauth<auth""mid""<beapi
-apimidwrapauthauthauthauthauth_authauth<mid_mid_use_mid_fn_wwrap_wrapperauthauthwrapper_wrapwrap_auth_wrapwrap_wrapwrapauthwrap_authuseruser_wauth.wrap_authauth_wrapper_wrap_auth<wrap_wwrap_fl_authwrapper""<mid<__<w_flwrap_wrapwrapwrapper_defauth_bemidwrapperwrapper_wrapauth<wt_wselfauthwrapper
-wwrapmidmidauthuserwrapperauth_authauthmidauthmid_username_wrap__username_midapiwrapperauthwrap_authcauth
+fU<WA
+<<AUT
+user<W,U
+file,and<user<user
+I andWRA and<S_USERW andS.
+UFAMES
+THEWA
+THEWESSAWALLSSP<
+STPA_WE
