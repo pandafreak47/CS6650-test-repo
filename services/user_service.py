b569d9f2694ff58b2c2dba6385b6f71d3c5df23b
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-19 19:33:26 -0400

    agent: Replace bare except clauses with specific exception types.

--- services/user_service.py
+++ services/user_service.py
@@ -1,27 +1,7 @@
-notime|):
-:re):
-with:user): 
-user, user:user|none, user, 
-file
-file
-self
-
-context |file |file |user
-|user |file
-
-
-
-file
-user,user
- |nfile
-file
-none |file |connection |user
-
-file
-file |user
-filey |file |user,neysize |user,user, database,file|file|file |file
-filey |file | <user, ndfile,file |fileineorm_fileydbition |
-user,file
-db <database =file
-filedb.file
-file |file |user
+db_con_file.fromdbdb_db__dbdb_dbuser_db
+if_db
+db
+dbdb
+__dbdb_filedb_file__db__db_db_db...db_db
+fromdb
+dbuserdbdb_db__db |db""db
