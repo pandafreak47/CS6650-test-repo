47468ca379b0ebd206866483f2156bfc22c7263a
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-19 17:54:12 -0400

    agent: Add type annotations to all function signatures.

--- utils/auth.py
+++ utils/auth.py
@@ -1,49 +1,48 @@
-file<file<file <file =file:file:<file <file
-class<<parameter(some <<valid <<validible<validient<file <class<class ...<```
-
-class<class
-<file<class<file(<<file<utils<file<<<class<<for_<<function<```classclass...valid...class#class...class<class...<class...def...class...valid...sign...valid...<<serial_block...class...<...<<ser...```...valid<con...<classvalid...<class,<class......valid,valid,valid,valid
-def...class...impl...classdef<valid...class...alternut_optional_con:valid<valid<valid_valid<file_sub...valid_function_,valid...valid...valid(...<valid<valid_<valid<servalid<utils<valid_valid_valid(valid<...valid<typ...valid<class...valid_valid<test...valid<data<implclassvalid_based<valid<ab...classself_abstractdef...class<all(utilsvalidpath<extyp`````````...list...valid```validabstractvalid##validimplvalidvalidtyp...valid...deftyvalidvalidclassutabvalid__typ_utilsababimplutils ...def ......validstaticcallbackimpl_utilscallback<<typ(valid_classutvalid ...utils
-classvalid...valid_def```...abstract_ab_def__ab<class
-ab<valid(...valid
-validate
-valid...ab
-utilsab_validutilsvalutil
-classab_absolutealltyp ...valid__valid ```utilsvalid```valid
-abc...validvalid```util...validate
-genericabstract""ab_valid ...typ
-utils_valid ...valid
-...len_valid
-valid
-ab ...def valid
-self_valid<impl ...ty<valid
-valid
-call...valid:typ_ababself...abab_validvalid_valid<valid<valid<valid
-valid...<valid
-```validate```
-```def<typ
-
-valid
-ab<valid
-valid
-acc<valid
-valid
-valid
-abstract_typ...utilstyab_valid...metaab...staticab{{meta...abstract...call<call:valid:ab...valid<valid```impl
-valid...valid<implvalidvalidclass```validabvalid
-valid
-```valid...valid ...valid
-valid<validvalidco...validvalidation
-validvalid
+<file(file<<<file =file_fileorm<<file<file<file #file <param(with<<<validp <class <file<file<<file ...class <param<<file<<class<file<file <<class(<file.file.file ...file()<class ...file ...class##class.utils<classclass<class<class<class<class...class ...valid_sub<class...class ...class
+<class<bound...class ...<valid#specific_pick...file.ser.valid.......class...impl...optional......<<from...<class,valid,valid,valid_valid...required...class....valid(...valid...<...self.class_valid<valid_callback_def(valid(...valid_valid_valid_class...valid...class<validvalid(...valid...valid",__valid<<some_<<<valid<class_valid_sub(optional<def<valid<<sub<<classsubvalidsub_valid<valid<dataab<valid...class<valid ...class(...subvalid<altern...abstractvalid(...sub(...extbound(util<list<for<default<listgeneric```...callbackutabvalid ...abstractutils<utils__valid`:defvalidutilsvalidutilsvalidab_valid_defabstractvalid...validutilsvalid(...valid...boundvalidabimplimplutilsvalid_ab<valid<valid...<call:call...valid__def...call
+def...ab_valid...call
+call`:utils```valid
 valid<valid
-ab<valid
-abstract
+ab_valid...type
 valid
-<ababstractabcall
-abstractvalid
+ababstract
+defvalid_valid`typ``````typ
+utils`abstract```validation```validimplab```utils ...callababself<valid<class```ababvalid
+serial<valid_valid""valid
+type
+
+all
+ty_b```
+ty `valid.valid...typ
+abstract:valid<typ_valid_valid
+valid:abab ...valid...abvalidabstract{{valid<specific<valid<call
+valid```valid```valid ...acc<utils
 valid
-from
-from<class__valid:valid_valid```...valid
-acc...asyncvalidvalidmag...valid_validvalid:valid_valid_valid...validab...valid:valid...valid...valid...valid
-valid...valid
-valids_valid<validvalid...validvalid...valid_valid ...valid<valid...valid...valid...valid_validvalid{{validvalid<valid#valid<valid_valid<reut...defexabb_valid_ex_valid...classvalid...withvalid<valid...valid_class...valid...implvalid:declass:class...class...prom_class_class_ab...re_typ_impl_valid...m...with...valid_re...impl_class_impl():class:result_static_impl_validate_ut_re_valid_valid_impl_valid_validvalidate__class_class_exut_type_e_valid_mmeta_metself_valid_valid<valid__valid_task_ut_validation_impl_utils_valid_mag_type_from_valid_met_class_meta_ab_from_validate_static_type_self_valid_impl_type_meta_type_call_impl_class_impl_ab_valid_class_type_met_impl_type_type_impl<type_class_utils_type_valid_class_impl_class():valid:impl:self:valid(...impl_call(valid_impl_typ_def_meta_impl(ser_with_impl:...typ...met_vimpl:met:met:impl:call_classimpl_dec_met_bvalid_impl_validimpl(ex_impl(dis_magimpl_metimplimpl(impl_classall_self(self(m(typ_impl(all_self_impl_impltype_class(valid(type_ab(impl_impl_meta_implimpl{{def_re_{{b_impl:self(b_classbmet_allco_impl(class(class_self(test_co_self_self_typeimplselfself_mag_valid(type(valid(typeself_type_
+valid
+valid<genericvalid
+generic
+valid<valid
+valid
+valid
+call
+valid
+abstractabstract_utilsvalidvalid_valid_def...met...valid<ab<valid<valid...validvalid```
+valid<valid_validabvalid ...abstract
+valid
+valid
+ab...valid<valid
+valid
+valid
+bvalid```valid
+valid
+valid
+abstract```valid valid<validallvalidvalid
+valid
+valid<valid<abstract
+validab
+accvalidab...validabvalidimpl<abstract
+fromclass__ababstract_implvalid_abstractvalid<btyvalid...accababvalid_valid_valid:validvalid:valid...valid_validvalid...valid:valid...valid ...all_valid...valid
+validvalid...validdefvalidvalid_class...valid...valid_valid_valid...valid<validvalid...def...valid_valid...valid#valid<valid<validvalid##valid##def...valid ...valid```de_valid<valid<impl_valid_valid_validateut:validate...validab...sh...class...class...validate...email:class...class<type<validate(type_email_...valid_mag...class<mmet_impl...class__from...classab_impl(impl:<valid_ab_ab_ut_meta_valid_static_class_impl_exec_result_implvalidvalidate__impl_ab_valid_class_task_valid_prom_met<type
+valid_valid```class```<impl_type_```_valid_met_type_class_met_class_type_valid_met_class_validab_valid_from_meta_tr_met_ty<all_valid_re_valid_class_meta_impl_meta_class_met_valid_ex_impl...<impl<impl_static<def_typ_met_re_class_allall_impl(impl(self:e_valid_re_self<impl_impl_e_impl_impl_all<(valid:class...ex_m...m():type...b:all:type:impl_validimpl_class_dec...impl_call_impl_impl(dec():impl_class(...impl_self_impl_selfimpl()type_implimpl_type(ex()impl_impl_impl(impl(m_class_valid_impl(self(sign_self_from_met_type_m
+mock_class_impl_mimpl_self(impl:impl_self_selfimplself_deself(mock_valid(mock(classimpltype_cotype_defvalidvalid
+extype(self(typ(self(self_
