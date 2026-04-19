c67d7759cc131517099ce82da2d5e5a649c4eec7
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-19 18:13:53 -0400

    agent: Add a logging statement at the entry point of each public fu

--- api/middleware.py
+++ api/middleware.py
@@ -1,68 +1,28 @@
 <file path="utils/auth.py">
-<file path="api/middleware.py">
 <file path="api/middleref.py">
-<file(file<file<<<file<<file...file ...class <file<file<file <<file ...class <param(with<<<validp <class <file<file<<file ... <file...call
-<class<bound...class ...<valid#specific_pick...file.ser.valid.......class...impl...optional......<<from...<class,valid,valid,valid_valid...required...class....valid(...valid...<...self.class_valid<valid_callback_def(valid(...valid...valid",__valid<<some_<<<<valid<class_valid_sub(optional<def<valid<dataab<valid...class<valid ...class(...subvalid<altern...abstractvalid...class....valid...class<valid ...class(...sub(...extbound(util<list<for<default<listgeneric```...callbackuta...valid...callback...valid...class...valid_def...callaba...call...valid__def...call
-def...ab...call
+<file path="api/middleref2.py">
+<file path="api/middleref3.py">
+```
+<file(file<file<<<file<<file ...class <file<file<file <<file ...class <param(with<<<validp <class <file<file<<file ... <file...call
+<class<bound...class ...<valid#specific_pick...file.ser.valid.......class...impl...optional......<<from...<class,valid,valid,valid_valid...required...class....valid(...valid...<...self.class_valid<valid_callback_def(valid(...valid...valid",__valid<<some_<<<<valid<class_valid_sub(optional<def<valid<dataab<valid...class<valid ...class(...subvalid<altern...abstractvalid...class....valid...class<valid...call
+def...ab_valid...type
+valid
+aba...valid...abvalid....valid...call
 call`:utils```valid
-valid<valid
-ab_valid...type
-valid
-ababstract
-defvalid_valid`typ``````typ
-abstract:valid<typ_valid_valid
-valid:aba...valid...abvalidabs...ab_valid...type
-valid
-aba...valid
-bvalid```valid```valid ...acc...valid...valid...valid ...all
-ty_b```valid
-ty `valid.valid...typ
-abstract...ty_b``````typ
-utils`abs...valid...valid...valid...valid ...bvalid```valid
-valid
-valid
-valid
-call
-valid
-abs...valid...valid...valid...valid ...bvalid```valid
-valid
-valid
-abs...valid...valid...valid...valid ...bvalid```valid
-valid
-valid
-bvalid```valid
-valid
-valid
-abs...valid...valid...valid...valid ...bvalid```valid
-valid
-valid
-bvalid```valid
-valid
-valid
-abs...valid...valid...valid...valid ...bvalid```valid
-valid
-valid
-bvalid```valid
-valid
-valid
-b...call
-call`:utils```valid
-valid<valid<class```aba...class<valid<valid...call
-valid...call
-call`:utils```valid
-valid<valid...class...class...class<valid<valid>vali...call
-valid...call
-valid<valid...call
-valid<valid``````valid<class<valid...call
-valid<valid<call
-valid```valid<valid...valid<valid```typ```valid<valid```
-typ
-call
-valid```valid...class<valid<valid```valid``````class<valid<validvalid``````valid
-<valid```valid<valid>valid```class<valid```valid```valid```valid```valid```valid```
-valid```<valid``````valid```valid```valid```valid```valid```````````valid```valid``````<valid```valid``````valid``````valid```valid```
-`````` ```valid``````valid``````
-valid`````` ```valid```````````` ````````````<valid````````````<`````` ````````````<```<``````valid>
-
-`````````<<string#<valid``valid> <valid:valid<````
-````````````````````````````````````<```<```<``````<
+valid``````typ```typ
+abstract:valid<typ_valid_class``` ```typ...call
+def valid_def<valid_val__class<class<valid`````````valid```valid```typ
+absvalid``````call
+defvalid```valid```valid<class<valid<valid``valid<valid<valid_typ_valid```call:def<valid<valid<valid``````valid```class
+call```valid```valid```valid```typ```valid<valid<valid```valid``````valid```
+call``````valid``````
+```callvalid<valid<```valid`````<valid```val```valid```valid```valid`````````valid```valid```valid```valid```valid```valid``````valid````````````valid```invalid``````````valid``` ```valid
+```valid````````` ```val``invalid```val```valid```<``` ```<
+valid>valid```
+```<string>
+```valid<>>valid<<param<>><string```<<<```
+``````<<<<```<__with>```<<<<``>><<<<<<><<<<<<<<<<<<<<<<<<<<```<<<<<<<<<file<<<file
+```<<<<<<<<<<<file<<<<<<<<<<<<file <<<<<<<file<file<<file#file```<file<<<<<<<<file..<<file<<file,file_<<file<<<<<<file<<<<<<file<<<<file><<<<<<<<<<...<<<<<file<<<<file<file<<file.<<<file<<<filefile<file<<<<<file>file<file<file<file <filefile>>file<>file <filefile<file<filefile<<file<<filefile<<file,filefile<file<file.<file<<file<classfile<<file<<filefile<file<filefile<file,file<file<<file<file <<file.filefile<<<file,file_<valid file<<<filefilefile<<file <valid <<<call<file<>file<
+filefile<><<filefile">filefile_file<user<filefile<file<<<file<<filefilefile<...file<>or_file_param,filevalid<file <<<with<<valid<<valid<<<<valid<file<validfile_file<file<><valid<and<file>andfile<classe<
+<andfile,call<valid<file...<
+<<file<file <user <e<valid<<>withfile<<```file_test<valid "<validfilefilefilefile.des<<th::<<
