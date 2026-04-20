06b3748aab6b5331f484d99aae130aa908d6e33e
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 10:55:49 -0400

    agent: Add an __all__ list that exports only the public API.

--- utils/templates.py
+++ utils/templates.py
@@ -4,52 +4,37 @@ from django.db import models
 from django.db.models import get_app, get_models
 from django.db.utils import IntegrityError, OperationalError
 from django.core.exceptions import ValidationError
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signal import pre_save, post_save
 from django.db.utils.apps import DjangoDatabaseErrorHandler
 from django.dispatch.dispatcher import Message
-from django.db.backends.base.base import (
-    BaseDatabaseFeatures,
-    create_db_engine,
-    create_db_engine_settings,
-    create_db_session,
-)
-from django.db.backends.creation import create_default_db_tables
-from django.db.backends.creation.utils import create_test_database_from_settings
-from django.db.backends.creation.utils import ensure_database
-from django.db.backends.creation.utils import ensure_database_exists
-from django.db.backends.creation.utils import ensure_database_from_settings
-from django.db.backends.creation.utils import ensure_database_exists_from_settings
-from django.db.backends.creation.utils import ensure_test_database_exists
-from django.db.backends.creation.utils import ensure_test_database_from_settings
-from django.db.backends.creation.utils import ensure_test_database_from_settings_with_replica
-from django.db.backends.creation.utils import ensure_test_database_from_settings_with_replica_with_schema
-from django.db.backends.creation.utils import ensure_test_database_from_settings_with_replica_with_schema_for_replica
-from django.db.backends.creation.utils import ensure_test_database_from_settings_with_replica_with_schema_for_replica_with_schema
-from django.db.backends.postgresql.base import PostgresqlDatabaseFeatures
-from django.db.backends.postgresql.creation import _postgres_create_database
-from django.db.backends.postgresql.schema import (
-    ADD_COLUMN,
-    ADD_COLUMN_WITH_TYPE,
-    ADD_COLUMN_WITH_TYPE_DEFAULT,
-    ADD_COLUMN_WITH_TYPE_UNIQUE,
-    ADD_COLUMN_WITH_TYPE_UNIQUE_DEFAULT,
-    ADD_COLUMN_WITH_TYPE_UNIQUE_DEFAULT_NOTNULL,
-    ADD_COLUMN_WITH_TYPE_UNIQUE_DEFAULT_NOTNULL_NULL,
-    ADD_COLUMN_WITH_TYPE_UNIQUE_DEFAULT_NULL,
-    ADD_COLUMN_WITH_TYPE_UNIQUE_DEFAULT_NOTNULL_NOTNULL,
-    ADD_COLUMN_WITH_TYPE_UNIQUE_DEFAULT_NOTNULL_NULL_NOTNULL,
-    ADD_COLUMN_WITH_TYPE_UNIQUE_DEFAULT_NULL_NOTNULL,
-    ADD_COLUMN_WITH_TYPE_UNIQUE_DEFAULT_NOTNULL_NOTNULL_NOTNULL,
-    ADD_COLUMN_WITH_TYPE_UNIQUE_DEFAULT_NOTNULL_NOTNULL_NOTNULL_NULL,
-    ADD_COLUMN_WITH_TYPE_UNIQUE_DEFAULT_NOTNULL_NOTNULL_NULL_NOTNULL_NOTNULL,
-    ADD_COLUMN_WITH_TYPE_DEFAULT,
-    ADD_COLUMN_WITH_TYPE_DEFAULT_NOTNULL,
-    ADD_COLUMN_WITH_TYPE_DEFAULT_NOTNULL_NULL,
-    ADD_COLUMN_WITH_TYPE_DEFAULT_NOTNULL_NULL_NOTNULL,
-    ADD_COLUMN_WITH_TYPE_DEFAULT_NOTNULL_NULL_NOTNULL_NOTNULL,
-    ADD_COLUMN_WITH_TYPE_DEFAULT_NOTNULL_NOTNULL_NOTNULL,
-    ADD_COLUMN_WITH_TYPE_DEFAULT_NOTNULL_NOTNULL_NOTNULL_NULL,
-    ADD_COLUMN_WITH_TYPE_DEFAULT_NOTNULL_NOTNULL_NULL_NOTNULL_NOTNULL,
-    ADD_COLUMN_WITH_TYPE_NOTNULL,
-    ADD_COLUMN_WITH_TYPE_NOTNULL_NULL,
-    ADD_COLUMN_WITH_TYPE_NOTNULL_NOTNULL
+from django.db.backend.creation import create_db_engine, create_db_engine_settings
+from django.db.backend.creation.utils import create_test_database_from_settings
+from django.db.backend.creation.utils import ensure_database
+from django.db.backend.creation.utils import ensure_database_exists
+from django.db.backend.creation.utils import ensure_database_from_settings
+from django.db.backend.creation.utils import ensure_database_exists_from_settings
+from django.db.backend.creation.utils import ensure_test_database_exists
+from django.db.backend.creation.utils import ensure_test_database_from_settings
+from django.db.backend.creation.utils import ensure_test_database_from_settings_with_replica
+from django.db.backend.creation.utils import ensure_test_database_from_settings_with_replica_with_schema
+from django.db.backend.creation.utils import ensure_test_database_from_settings_with_replica_with_schema_for_replica
+from django.db.backend.creation.utils import ensure_test_database_from_settings_with_replica_with_schema_for_replica_with_schema
+from django.db.backend.creation.utils import ensure_test_database_from_settings_with_replica_with_schema_for_replica_with_schema_for_replica
+from django.db.backend.postgresql.base import PostgresqlDatabaseFeatureSets
+from django.db.backend.postgresql.creation import _postgresql_create_database
+from django.db.backend.postgresql.schema import (
+     ADD_COLUMN,
+     ADD_COLUMN_WITH_TYPE,
+     ADD_COLUMN_WIThET_TYPE_DEFAUlt,
+     ADD_COLUMN_WIThET_TYPE_DEFAUlt_NOTNULL,
+     ADD_COLUMN_WIThET_TYPE_DEFAUlt_NOTNULL_NULL,
+     ADD_COLUMN_WIThET_TYPE_DEFAUlt_NOTNULL_NULL_NOTNULL,
+     ADD_COLUMN_WIThET_TYPE_DEFAUlt_NOTNULL_NOTNULL,
+     ADD_COLUMN_WIThET_TYPE_DEFAUlt_NOTNULL_NOTNULL_NULL,
+     ADD_COLUMN_WIThET_TYPE_DEFAUlt_NOTNULL_NOTNULL_NOTNULL,
+     ADD_COLUMN_WIThET_TYPE_DEFAUlt_NOTNULL_NOTNULL_NOTNULL_NULL,
+     ADD_COLUMN_WIThET_TYPE_DEFAUlt_NOTNULL_NOTNULL_NULL_NOTNULL,
+     ADD_COLUMN_WIThET_TYPE_DEFAUlt_NOTNULL_NOTNULL_NULL_NOTNULL_NOTNULL,
+     ADD_COLUMN_WIThET_TYPE_DEFAUlt_NOTNULL_NOTNULL_NULL_NOTNULL_NOTNULL,
+     ADD_COLUMN_WIThET_TYPE_DEFAUlt_NOTNULL_NOTNULL_NULL_NOTNULL_NOTNULL,
+     ADD_COLUMN_WIThET_TYPE_DEFAUlt_NOTNULL_NOTNULL_NOTNULL_NOTNULL_UNKNOWN_TYPE
