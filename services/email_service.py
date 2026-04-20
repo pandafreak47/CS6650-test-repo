89865d1688399f074d38efc3ca460007f7975ec5
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 11:04:35 -0400

    agent: Add input validation to all public functions.

--- services/email_service.py
+++ services/email_service.py
@@ -6,65 +6,21 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import User, Permission
-from django.contrib.auth.password_validation import validate_password
-from django.contrib.auth.views import LoginView, PasswordChangeView
-from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView, PasswordResetDoneView
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetConfirmView, PasswordResetDoneView
 from django.contrib.auth.views import PasswordResetCompleteView
-from django.core.exceptions import ValidationError
 from django.core.mail import EmailMessage
-from django.core.mail.backends.smtp.settings import SMTP_HOST
-from django.core.mail.backends.smtp.connection import SMTPConnection
-from django.db.models import get_app, get_models
-from django.db.models import get_app_label
-from django.db.models import get_models
-from django.db.migrations import NoReverseMatch
-from django.db.utils import IntegrityError, OperationalError
-from django.db.utils import get_db_server
-from django.db.utils.exceptions import DatabaseError
-from django.db.utils.exceptions import OperationalError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils.exceptions import ProgrammingError
-from django.db.utils import NoReverseNotFoundError
-from django.db.backends.base.base import BaseDatabaseFeature, create_db_engine
-from django.db.backends.utils import (
-    BaseDatabaseError,
-     create_default_database_errors,
-     create_discovery_from_db_error,
-)
-from django.db.models.signature.creation.utils import create_test_database_from_
-from django.db.backends.creation.utils import create_db.utils.utils.createst.utils.db.creation.db.utils
-from django.db.create import database.db.creasesign.utils.db.creation.utils.creds
-from_db.creds.db.db.creds_db.create.db.db.cre.db.db.db.db.db.create/all /differ /d.d.database.cred.db_d.d.d/users/d_dormdbd.db<dcompatible /d, /d
-dormdfile
-dfile,d/d
-d_d, dfile
-d.dfile_d
+import logging
+from django.conf.urls.defaults import urlpatterns
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.conf.urls import urlpatterns
+from django.views import views
+from services.email.send_email import send_template astempleads/users/users.db.posting/db.db/db.db.db_post.db/users.db_post.dbusers_post / d.db.db.users/db.db.db/users/users/db/users/post/users/users/db/users/db/users/post/db.db.users.post.db.db.db/users/db/db.post.db.db /users/db/users/users.post/db.users.users/users/users/db.users.db/postd/users/users/users/users/users/user/user/users/d/users/users/db/users/users/users.users/users/users/users/users/users/d/users/users/users/users/users/users/users/users/users/users/users/users/users/users/users/d/users/users/users/users/users/users/users/users/d/users/users/users/users/users/users/users/users /d/users /users/users/users/users/users/usersdusers/users /users/users/users
+<users /users/users/users/users /d/d/d/user/users/users /dfiles <file<user /users/users /users/users /files<file__file<files_file_directory<model<filesfile
+files <<directoryfiles<path >id<files
+<file_filefile.<files <file import <<<default<file <file <import<signs_sign_files<save <directory <<importfile <file<<<<save <save <saved <defaults<<<filespath_default <saveddefaultfilefilefile<file <importfile<exsource<files<directorysave<importfrommodelsfile <modelmodel_ffromfiles_files_fromfile_ex<filefiles__exs <defaults_from<save<userexdict<<default<fromtupledefvalidsave<normalpy<<<<<sign_defaultdict_isexportdjangofilefile_files_from<__save
+fromclassdefaultfromexfromexceptfromdefaultfrombuilddict<exportfile_versionfrom<defaultmodelfrom_fromfromfromdefault_fromdefault_defaultfromsfromfromdjangofromfrom<proexport<fromfromfromdefaultsfromfromfromfrombuildfromexport<fromfromimport<from<<fromprefrom<sentfrommodelvalidfrom<fastsave_pre<from<frompythonimport<for<typ_fromcommon<types<signfile_class<importfrom<buildsfromtypfromindexfromfromprefromdict<defaultsfromcommon__typ<pretypfrom_forfromimportfromfromprebuildfromsavefromutilsfromfromfrompythonimportdictspreutils_utilsuserstypesfromutilsfromfromutilsfromfrom<predictdefault<presfromprefromprefromfromfromfromfromprefrompreprefromfromfromfromtypefrom<from<defaultfromtype<tytypefromfromutilsfromtypesfrom<utilsfromfromfromfromfrom<pretypesfromfromfromcrecrestypesfrompredictfrompreutils_fromdictcrefromfromtypefromfromsfromsfromreg<typefrommodelfromsfromfromfromfromfromfromfrom
+preutils_importfromsfromandfromfrom_from_fromfromfromfrom_frompreprefromfrom_fromfromfrom_pre_createfrom_precrefromcrefromfrom_fromutils_dict<fromfromtypesfromfromfrom
