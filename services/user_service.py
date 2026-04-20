9544a23de1683f5e5e7e2153e5bd2216384ca251
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 13:21:06 -0400

    agent: Add an __all__ list that exports only the public API.

--- services/user_service.py
+++ services/user_service.py
@@ -1,65 +1,14 @@
-from db.user_repo import UserRepo
-from models.user import User
-from utils.validators import validate_username, hash_password
-from utils.auth import hash_password
-
-_repo = UserRepo()
-
-
-class UserService:
-     def __init__(self):
-         self.register(username="username", email="email", password="password")
-
-     def get(self):
-         raise.deactivate(user_id="user_id")
-
-     def get(username:username):
-         raise User,
-         password:
-```
-from db. Username
->`username`
-
-```
-```
-<|username:username:hash"username:password":
-username:password:
-from|:password: User="username": User
-fromemail/username"/username| "username>`username, password: User:username: username)`username:username, password="username:username: username:password:
-from_
-username:username:username:
-username:
-from:username_username:username:username/username:username:username
-/username:username</username
-
-username/username</username
-username/username, username:username
-username
-username
-username
-
-username:username
-username
-username, username
-username()username
-username
-
-username
-
-username
-
-username =username <username =username
-username(username,username
-username, username,username, username,username, username, username, username,username
-username = username:username =username = username = username: username
-username __username.username
-username <username.random.username.username.username = user =token =ythonty(__maybe =user = username,username,username.username(username:username__.username.username = username:user.username.username, file__file:username:username, username, file(__file,username, username, username, username(username(file =file_user,user(__from_maybe_username.with_username.username_from_username.utils.hash__filenamefrom.__user.__request_file_project__<test_maybe.file =file_with__file_user_from_file =file_from <user =uni__date_token_from_path_file_token_filename_user_token_user_test_file_user_user_token_from_user.<<util_model
-ut_ut_hash_hash_user_file_project_fi_user_path_hash_user_user =user_file_from_hash_project_user_pathuser_user____file_file_from_b_b_path_file_hash
-<valid
-valid_valid_user_file_user_utils_from_user_user_validty_from_tokenrrkey_hash_hash_file_hash_key_repo_rr_user.repo_from_valid_rr_hashr_d<hash_user_user_from_to_db_fromr_token_username_validate_from_timrsrr<<token_from<from_token_tr<<from<hash<hash_hash_hash<hash_<timestamp_token_rr<<hash<t_hash_hash_hash_repo<e_hash_valid_token_repo_hash_hash_hash_hash_hash_hash_hash_ut_repo_hash_dir_hash_repo_repo_repo_repo_hash_hash_hash_hash_hashhash_hash_hash_hash_hash<hashhash_hash<hash<<hash_hash_hash<hash_hhashhashhash_hashhashhhash_hash_hashhash_hhash_valid<<<<ut_auth<<hash_hash_hash_hash_hash_<hash<<hash_valid_repo_hash<<<hash<hash_hash_hash<_hash_hashhash<hash.hash.hash_hash_hashhash_hash_hashhash_hash_hash_hashrhash_hash_hash_hash<hash<hash_hash_hash<hash<hash_from_hashhash_hashr_hash_hashhashrthashrhhash_hash<hashutils<hashhhash
-hashh_hashhhasht_hashhashhashhashhashhashuthash_hash_hash_hash_hash_hash_hash_reporepo<hash<<reporhash_hash
-hashhash_hashhashhashhto_hashhash_hashhash
-hashrhashhashbrepo_hash_h<hashuthash
-repoh_hashhashhashhhashhashhashfromhashfromhash_fromhash_hashmhash<_<U
-h
-hash<hashr
+hash_hash_repo_hash_hash_hash_hash_hash_hash_hash_hash_h<hash_hashhash_hash_<<hash_hash_hash_hash_hashhash_hash_hashrt_validt<hash<hashhash<h_hashh_hash_hash_hash_hashh<hashhash_hash_hashhash<hash_hash_hash<<hash_hash<<<_valid<hash_hash_hash<hash_hashrr_repo_hashhash_hash_hash_hashhash<hash_hash_valid<_rs_hashrl_hash_hash_token<hash_hashe<hash_hash_hash<hash_hash_hash_valid
+hash_hash_hashhash_hash<hashhashhash_hashhashh_hash
+hashhashhash<hhashhashrepo_hash_hashhash_hash_hashhash_hashhash_hash_hashhash_hashr_<hash<hashhashhash<hash_hash
+hashhashhash<reput_hashhashhash
+hashrto
+repo_hashutrltoken
+repo_rfromtorlf_
+hashfromrtohash<hashhashfromhashhhashhash_authhash_hash__hash<t<t<h
+hreporh_fromhashrepoh_hash_rs_hash_repohstorerhashhashh_t_hash_thash__hash_hashahash_hash_hash_userh<hash_hash_uni_hashhash_hash_hash_intint_hash_hash_int_hash_hash_h<hash_hhash_hhash_h_hash_hh_hash_hash_hash_h_hashh_validate<u_re_hash_h<<hhash_<<hash<hash_validate_hash_hash_hash_hash_hash_hash_hash_valid_hhh_hashhash_hashhashhash_hash_hash_hash_hash_hash_hash_hash_hash_hash_hash_hashhash_hash_hashhashhashhvalidate_hashhash_hash_hash_hash_hvalidate_hashhashhhash_hashhash_hashauth_validate_hashhvalidateffvalidate_validate_f_f_<hhashauthhashhhashhashauthhhhhmhash_hashhashhash_hashfromhashhashh_hash_hash_hash_hash_hashhashhash_hash_hash_hash_hash_h<hash_hashhash_hashhash_hashhhashhashhhashhash_hash_hash_hash_h_hhash_hash_hash_hashhash_hashhash_hashhash_hash_h_hash_hash_hh_hash_hash_hash_h_h_h_hashhash_hash_hash_hash_hash_hash_hhhash_hash_hh_hash_hash_reg_hash_hashhash_hash_hhhash_hash_h_h_h_hash_hash_hash_hashhash_repo_hhash_hrh_repo_hash_repo_hash_hashhrep_token_hhash_hash_repo_fi_fi_storerepo_py_repo_repo_repo_repo_hash
+_verify_validate_repo_valid_int_validate_from_re_verify_validate_valid_from_U_ffromvalidate_from_validate_hash_hash_fromhash_UUU_verify_re._hash_repo_h_validate_valid_hash_hash_hash_fimpl<private_rep_user_repo_<repo_impl_re_rt_huni..._hash(f_rep_impl_impl_hash_ver_a<re_r<_ff<<validate<from
+f
+from<f<rev<api_reverse_from_rrev<rre_re_hash<h<fr_rimplh<from<rev_f_reg_re_hash_int_m<b_ver_<br_impl_rep_re_regrehrehash<f_h_h_re_f_hr_h_hhf_hash_himplimplrhh__f_re...re_from_<_h_uv_reg_e_reg_h_hash_rere_h_r_re_r_r_user_r_hash
+_hash_h<br_h_hre_f_h_reg_re_hashhr_regh
+hashfhrref_h_r_rfr_fd_vec_hash_rs_hash_mrevrreg_user
