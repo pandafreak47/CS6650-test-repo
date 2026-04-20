5737aa17b0e95fab73301c9770ea0e7f064cf00a
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 01:48:39 -0400

    agent: Refactor any magic strings or numbers into named constants.

--- main.py
+++ main.py
@@ -3,41 +3,21 @@ from http import HTTPStatus
 from utils.auth import verify_token
 
 class AuthError(Exception):
-      status = HTTPStatus.UNAUTHORIZED
+        status = HTTPStatus.UNAUTHORIZED
 
 
 @wrap(fn)
 def require_auth(fn):
-     @wrap(fn)
-     def wrapper(*args, token: str = "", **kwargs):
-         if not token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer "):
-             raise AuthError("Missing or user /auth") fromauth
-         username, email, body = "body", "html\n"
-         current"
-     return "current\nhtml\n"
-     for "current\nhtml\n"
-     current\r\n"current"/"file" email"/"usersign /file"/"current\r/auth/sign / auth/"
-     user" /current/current/ /html,auth" email /html/current/current/current/email / html" /current/current"
-from/current/from/json/current,html/current/ //current/authentic / / / / / / / // / / / / / /html / / / / / / < / / / / / / / / / / / / / / / / / / / / / /json /user / / / / / // / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /user / / / / /auth / / / / / <file /file /file /file /file /user /user >user,user, user, user /user, user,rou
-
-from <params.user._user
+      @wrap(fn)
+      def wrapper(*args, token: str = "", **kwargs):
+          if not token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token or token: raise AuthError("MissBearer ") from token("MissBeare " "Bearer"Beare("Beare "Beare") @ "token from" "Beare "Beare "BeBe "Bearr "Be "Beare "Be "BeBe "Be "Beare "Bearp "BeBeare "Beare" "Beare "Be "Be "BeBeBe "BeBeBe "BeBeBe "BeareBeBeBe "BeBeBeBeBe "BeBeBeBeBeBeBeBBeBeBeBeBBeBeBeBeBeBeBeBeBeBeBeBe>BeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBeBe,user,userps -> fromBeBeBeBeBeBeUser<fileBeBeBeReUserUserSe
+(user
+user<user >user <user<login.author <UserBeUserUser <users<users<user <userUserUserUsersUser
+userUser
+FileUser<usersUser<user <userUser<usersUserUserUserUser -><userUserUserUserUserUserUserUser<user_users<UserUserUserUser<userUser<useruser_users<users<<users
 <user
-response
-user
-user<user <user <user_file.user >user
-user <tokenauthpy <user <user
-<auth <user<user <user <user
-auth <user
-from <request -->user ->from ->user <user<user ->users -><user ->userauthuser ->user <user ->users <users
-<user <user_authuser.<serveruser<user_auth<user
-token
-<<<<<<<<<webrides_user
-users
-users <users<<user =user_user_user<<auth_user<users<<user_user_user_user_users_user`,user_apiuser
-<user<user_foruser_user._user<auth_users_user_foruser<<login<fromauth_usersauthuser<user_user._users__user._authauthuser__fromuser_users_users_usersfromuser_users""from<auth_from<auth_user<users_users<auth_users<user_auth_users_users_users =<user_send```
-from_path_router_<users
-<from<users_users_auth<users<user<userapi_user<auth_authapi<auth<send<send
-auth<auth__send_files_usersauth_files__users<<users_users_<users_send_auth_authauthauth_users_users_auth<auth_auth_,users_auth_auth_user_send_users_auth_email_users_auth_auth_users_user_api_auth_auth_router_users_auth
-_router_mid_router_auth_auth_auth_auth_auth_auth_auth_email_auth_auth_authauth_auth_auth_auth<router_auth_auth_auth_auth_auth<email<auth<email__authauth_router<auth
-authauth_router_auth<auth<api_auth_auth_<auth__users_auth__email{{_users_authauth_auth_auth_auth_auth_authauthauthauth_auth_auth_send_authauth_mid_auth
-auth
+user<auth<<user
+<<userrides_<<user_<user<user_users<user_userUserMail_user._send_send_users <user_send_user_users<users_user_user<userauthuser ->users_<user__<user_user_send<users_userapi_user_user_users<authenticauthauthauthenticlogin_loginuser<<userapidbsend ->user_auth__from<userauthuseruseruserauthauthfromusersend""users_usersapiuser<users<user_users_users
+send<useruser<send_auths<from_from_user_users<user_<<<auth<router_auth<<<<authapi<user_fromapi<auth__<auth<<api<email__auth<auth__router<from__author_api_login_authentic<user_send_user_users<__auth_users__auth_auth_auth_auth_authauthauth_authauth_auth_users_users_authauthauthauth_auth_authauth_users_email_auth_api_auth_users_router_auth_auth_auth_user_auth_auth<auth_auth_auth_send_auth_send_api_auth_email_router_auth_auth_auth_auth_auth_auth_auth_auth_router_auth_users_auth_auth_router_auth_users<users<usersauth_auth
+<auth_users<auth_auth_auth<router_auth<auth_email_auth_authauth
+auth<auth<auth_midauth""auth_email_mid_authauthauth<auth_auth_mid_auth_
