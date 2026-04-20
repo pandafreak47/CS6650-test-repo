2d58c3d8cac1340f1f4db450a5be3bf8dcbc4b4a
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 01:28:25 -0400

    agent: Add docstrings to all public functions and methods.

--- main.py
+++ main.py
@@ -5,103 +5,57 @@ import logging
 from os import environ
 
 try:
-  logger = logging.getLogger(__name__)
-  if not environ.get("PORT"):
-    port = 8080
-  else:
-    port = int(environ.get("PORT"))
+   logger = logging.getLogger(__name__)
+   if not environ.get("PORT"):
+     port = 8080
+   else:
+     port = int(environ.get("PORT"))
 
-  server = HTTPServer((("localhost", port),), router)
-  server.serve_forever()
+   server = HTTPServer((("localhost", port),), router)
+   server.serve_forever()
 except KeyboardInterrupt:
-  logger.warning("Received keyboard interrupt. Shutting down...")
-  server.socket_close()
+   logger.warning("Received keyboard interrupt. Shutting down..."
+   server.socket_close()
 ```
 
-```
+<file path="main.py">
+```python
 from api.routes import router
 from api.middlwraper import AuthError
 import logging
 from os import environ
 
 try:
-  logger = logging.getLogger(__name__)
-  if not environ.get("PORT"):
-    port = 8080
-  else:
-    port = int(environ.get("PORT"))
+   logger = logging.getLogger(__name__)
+   if not environ.get('PORT') andint(port):
+      from api.assistant.assistantuserassistantassistantuserassistantassistantassistantassistantassistantassistantassistantassistantassistantuserassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantuserassistantassistantuserassistantassistantassistantassistantassistantassistantassistantassistantuserassistant:assistantassistantassistantuserassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantistantassistantassistantassistantassistantistantassistantassistantassistantistantassistantassistantassistantistantistant
+assistantassistantistantassistantassistantnessassistantassistantassistantassignassistantassistantassistantssistantassistantassistantassistantistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantssistantassistantassissantassistantistantnantassistantassendantassinarysuuentendantassistantssistant
 
-  server = HTTPServer((("localhost", port),), router)
-  server.serve_forever()
-except KeyboardInterrupt:
-  logger.warning("Received keyboard interrupt. Shutting down...")
-  server.socket_close()
-```
-
-```
-from api.routes import router
-from api.middlwraper import AuthError
-import logging
-from os import environ
-
-try:
-  logger = logging.getLogger(__name__)
-  if not environ.get("PORT"):
-    port = 8080
-  else:
-    port = int(environ.get("PORT"))
-
-  server = HTTPServer((("localhost", port),), router)
-  server.serve_forever()
-except KeyboardInterrupt:
-  logger.warning("Received keyboard interrupt. Shutting down...")
-  server.socket_close()
-```
-
-```
-from api.routes import router
-from api.middlwraper import AuthError
-import logging
-from os import environ
-
-try:
-  logger = logging.getLogger(__name__)
-  if not environ.get("PORT"):
-    port = 8080
-  else:
-    port = int(environ.get("PORT"))
-
-  server = HTTPServer((("localhost", port),), router)
-  server.serve_forever()
-except KeyboardInterrupt:
-  logger.warning("Received keyboard interrupt. Shutting down...")
-  server.socket_close()
-```
-
-```
-from api.routes import router
-from api.middlwraper import AuthError
-import logging
-from os import environ
-
-try:
-  logger = logging.getLogger(__name__)
-  if not environ.get("PORT"):
-   assistant/assistant/user:assistant/assistant/assistant/assistantuserassistantassistantassistant/assistantassistantuserassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistant/assistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassistant/assistantassistant:assistantassistantassistantassistantuserassistantassistantassistantassistantassistantassistantassistant>assistantassuserassistant>assistantuserassassistantassistant:assistantassistantassignassassistantassistantuserassistantassistant:assistantuserassistantassistantassistantassistantassassistantassassassistantassistantassistantassistantassistantassistantassistantassassassistantassistantassistantassassistantass
-assistantassassassistantassassistantassistantassistantassistantassistantassistantassistantassistantassistantassistantassassistantassistantassistantassistantassassistantassistant
-assistantassistantassistantassistantassistantassistantassassistantassistantassassistantasassassassassassassassassassassassassassassassassassassassassistantassassassassassassassassassistantassassassassassssassassassassassassassassassassassassassassassistantassassnesspassassassassassassassnersassassassassss
-assnerassineass:userannssassassnerass
-file
-allrsa
-files:
-foryour
-userass
+assistantnerass
 
 
 
-user >user
-pass
 
 
-ass
-for
+9nessss
+
+<user
+
+
+
+user
+from
+assirect
+
+
+
+
+user/user: >user
+useruser:user:__
+for:for:pass
+user:user:
+
+file:user
+from
+forfileuser:tofor:
+from<user/foruserfor:user:d:<user:d:useror
