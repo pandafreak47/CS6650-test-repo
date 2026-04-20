d02529cf3c900643c8ca18ffae678ada87ff718c
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-19 22:49:18 -0400

    agent: Add input validation to all public functions.

---
+++ services/user_service.py
@@ -0,0 +1,43 @@
+```python
+from typing import Dict, Union, Any, cast
+from fastapi import Body, Depends, RequestBody
+from pydantic import Field, validator
+
+from utils import database as DB
+
+
+class User(DB, dict):
+    def __init__(self, username:str, password,email:str) -> None:pass:
+        self._username = username
+        selfrsalt = Usersalt.split_user.split
+        self
+        usernamedepassplitsplit_us = username, emaildbs = password, email =username,email = database, password =database, user, password,email:passwordsplit,email = passwordsplit, usERS, username = password, password = password)
+
+db:password = password,str, User, password:password = database,us: __,email:s
+
+username:id:password
+pass,email,user, file,password = database, ufs(User,file = <random
+usernamef <<valid,str, US>
+<random>user,password>password <password,password
+path, User,password
+VALID,password, u,
+<password,username,password,
+<
+
+<data,uuid, u, U, u, u,pass, u, u, ufcase, U:fredf, u, ucase, u, U, u, u, uf, u, UF
+forbife <f<for <f <valid for for iff <valid
+f <patterns
+some <file <<<sent < < < < < < < < <<domain <domain <<cliente`, invalid<accounts:pass <for(valid__for<<<valid_file <<basedMail_f<captledients()email.for_valids <<valid <valid_<<valid <valid_valid_validizer_valid_sent <<<<<valid_<f <<valid_<<<account <<<<<<valid < <<<<email<<invalid<<f <
+<<valid<valid<<<<<valid
+<<<<valid<<<<<<<valid <<<<VALID <<<valid <valid<email_<email <<email_<<<<<<<valid_<valid<<<<domain <valid<<valid_<<valid<_<<<VALID_<<<<<<<<<valid<<<<_<email_<<valid_<
+<__<type_for
+<<b_<__<_ALL_<<<email_valid_<valid_valid_valid_<VALIDEVALID_FE_<<valid<<<valid <<valid_<<<<<valid_<<<<<<<<valid<<<<<<<<<email <valid#valid _email <valid<valid<valid<<<
+<<<<<valid_valid<<<<valid <<<<<<<<<valid_valid<<valid",__serial_<valid<valid_VALID____invalid_valid_typ_ex_<<<<type_invalid_valid_typ_invalid<VALID_valid_valid_valid_valid_valid_valid_valid_valid_valid_valid_valid<valid_email_types_valid_valid<valid_valid_valid_valid<valid_<valid_valid__types_valid_<mail<email<<<<<<<<<<<valid#valid<validation<valid<valid_valid<valid_<validate<<<<valid<<typ
+ty_email<<<valid_<mail_valid__valid<valid_valid<valid<<email<valid<valid_email._email
+valid_user<<valid_valid_valid<valid<valid<valid_valid_valid_valid_valid_valid_valid_validate[valid_valid_valid
+user_valid<valid_valid[:email_email<_valid_valid<valid<<<email_valid<valid<valid<valid_valid_valid<valid_valid_valid<valid_email_valid_valid_valid_valid<validty<valid_valid
+valid<email_<ty<e_valid
+typ_valid_valid_email_email_email<valid
+valid_valid_valid_valid_<valid<validty_valid<<valid_valid<valid_valid
+valid_using_valid<<<valid_valid_valid_valid<validty<valid_<valid
+EMA<valid_valid_valid<<valid_valid_VALID<valid_<
