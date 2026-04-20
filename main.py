73f87870315303641e99feca6600c84fe5a1a1af
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 01:45:14 -0400

    agent: Refactor any magic strings or numbers into named constants.

--- main.py
+++ main.py
@@ -1,62 +1,43 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from functools import wrap
 from http import HTTPStatus
-from os import environ
-from api.routes import router
-from api.middleware import AuthError
+from utils.auth import verify_token
 
-_users = UserService()
-_orders = OrderService()
-_email_service = EmailService()
-
-router: dict[str, callable] = {}
+class AuthError(Exception):
+      status = HTTPStatus.UNAUTHORIZED
 
 
-def route(path: str):
-     def decorator(fn):
-          router[path] = fn
-          return fn
-     return decorator
+@wrap(fn)
+def require_auth(fn):
+     @wrap(fn)
+     def wrapper(*args, token: str = "", **kwargs):
+         if not token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer ") or token.startswith("Bearer "):
+             raise AuthError("Missing or user /auth") fromauth
+         username, email, body = "body", "html\n"
+         current"
+     return "current\nhtml\n"
+     for "current\nhtml\n"
+     current\r\n"current"/"file" email"/"usersign /file"/"current\r/auth/sign / auth/"
+     user" /current/current/ /html,auth" email /html/current/current/current/email / html" /current/current"
+from/current/from/json/current,html/current/ //current/authentic / / / / / / / // / / / / / /html / / / / / / < / / / / / / / / / / / / / / / / / / / / / /json /user / / / / / // / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /user / / / / /auth / / / / / <file /file /file /file /file /user /user >user,user, user, user /user, user,rou
 
-
-@route("POST /users/register")
-def register(body: dict) -> tuple[int, dict]:
-     user = _users.register(body["username"], body["email"], body["password"])
-     return HTTPStatus.CREATED, {"id": user.id, "username": user.username}
-
-
-@route("POST /orders")
-def place(declarations) -> tuple[int, dict]:
-     order = Order(
-             Decorator({"GET": {"GET": [
-                 request
-                {"POST / {username:register}": {"body": Register([POST / {username:register} username)
-                POST /email: {"password": "username}
-            POST /username / email:server {
-            POST / GET} {"POST {POST /user": {"POSTserver / GET / Get /email / / GET {username, body / POST / / email / / POST / /username / /username /email / email / / / / / POST / / POST / / / / / / POST / / POST / / / / / / /  / / / / / POST / / / / /username / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /user / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / <p
- / / / / / / / / / / / / / / / / file / / / / / / / / / / / / / <user / / / / /file / file /user: ...user, user, user, user /user.token,userices <request <user -><user
-user
-response
-user ->user
-user <user /user/user <user.user.user
-user_file <user <user <auth <user <user <user <token<user <users
-token
-user
-<users<from<user.user <token->user <user -> <user <user <user <auth =>file <user <user
-<user_user_user_users
-user
+from <params.user._user
 <user
-<users
-user <users
-<request <user <user <<<<<<send_user_<user_user_users_authentic_user<auth_user_render_user_<user_user_users
-user._user<user_user<user_user_user_user_user
-user.user.user_user_send._user<auth_user<user_users._authenticlogin,auth_users__user__apiuser_user ->auth_users_user_authauth_user_from_from_send_users_users_users
-<users<user_users_usersuser<<<auth<from<authuser
-from```<author_router
-<user_router<auth
-from_user_<user_user_auth<users<auth<auth__api_user<auth_<api_users<auth
-send<users<email<auth<auth<router_send_users
-<auth_users_auth_users_auth_users_auth_auth_auth_auth_auth_users_authauth_authauthauth_auth_auth_auth_users_email_email_auth_auth_user_users_router_auth_auth_auth_send_auth_router_author_auth_auth_auth_send_auth_auth_auth_auth_auth_auth_auth_author_auth_auth_auth_auth<users<auth_auth_users_api_users_users_auth_authauthauth_email_auth<<api
-auth<<auth<auth_auth_auth_api<email_users_router_emailauth_users_auth_authauthauth_auth_auth_authsauthauth_auth_auth_authauth_auth_user_auth_users_midrouter
-author
-api
+response
+user
+user<user <user <user_file.user >user
+user <tokenauthpy <user <user
+<auth <user<user <user <user
+auth <user
+from <request -->user ->from ->user <user<user ->users -><user ->userauthuser ->user <user ->users <users
+<user <user_authuser.<serveruser<user_auth<user
+token
+<<<<<<<<<webrides_user
+users
+users <users<<user =user_user_user<<auth_user<users<<user_user_user_user_users_user`,user_apiuser
+<user<user_foruser_user._user<auth_users_user_foruser<<login<fromauth_usersauthuser<user_user._users__user._authauthuser__fromuser_users_users_usersfromuser_users""from<auth_from<auth_user<users_users<auth_users<user_auth_users_users_users =<user_send```
+from_path_router_<users
+<from<users_users_auth<users<user<userapi_user<auth_authapi<auth<send<send
+auth<auth__send_files_usersauth_files__users<<users_users_<users_send_auth_authauthauth_users_users_auth<auth_auth_,users_auth_auth_user_send_users_auth_email_users_auth_auth_users_user_api_auth_auth_router_users_auth
+_router_mid_router_auth_auth_auth_auth_auth_auth_auth_email_auth_auth_authauth_auth_auth_auth<router_auth_auth_auth_auth_auth<email<auth<email__authauth_router<auth
+authauth_router_auth<auth<api_auth_auth_<auth__users_auth__email{{_users_authauth_auth_auth_auth_auth_authauthauthauth_auth_auth_send_authauth_mid_auth
 auth
