fb7d5db651c85a11c402bdea01b53fb7cdc68e4b
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 13:17:29 -0400

    agent: Add an __all__ list that exports only the public API.

--- services/user_service.py
+++ services/user_service.py
@@ -1,26 +1,65 @@
 from db.user_repo import UserRepo
 from models.user import User
-from utils.validators import validate_email, validate_username
+from utils.validators import validate_username, hash_password
 from utils.auth import hash_password
 
 _repo = UserRepo()
 
 
 class UserService:
-    def register(self, username: str, email: str, password: str) -> User:
-        validate_username(username)
-        validate_email(email)
-        if len(password) < 8:
-            raise ValueError("Password must be at least 8 characters")
-        hashed = hash_password(password)
-        return _repo.insert(username, email, hashed)
+     def __init__(self):
+         self.register(username="username", email="email", password="password")
 
-    def get(self, user_id: int) -> User:
-        user = _repo.get_by_id(user_id)
-        if not user:
-            raise LookupError(f"User {user_id} not found")
-        return user
+     def get(self):
+         raise.deactivate(user_id="user_id")
 
-    def deactivate(self, user_id: int) -> None:
-        self.get(user_id)  # raises if not found
-        _repo.deactivate(user_id)
+     def get(username:username):
+         raise User,
+         password:
+```
+from db. Username
+>`username`
+
+```
+```
+<|username:username:hash"username:password":
+username:password:
+from|:password: User="username": User
+fromemail/username"/username| "username>`username, password: User:username: username)`username:username, password="username:username: username:password:
+from_
+username:username:username:
+username:
+from:username_username:username:username/username:username:username
+/username:username</username
+
+username/username</username
+username/username, username:username
+username
+username
+username
+
+username:username
+username
+username, username
+username()username
+username
+
+username
+
+username
+
+username =username <username =username
+username(username,username
+username, username,username, username,username, username, username, username,username
+username = username:username =username = username = username: username
+username __username.username
+username <username.random.username.username.username = user =token =ythonty(__maybe =user = username,username,username.username(username:username__.username.username = username:user.username.username, file__file:username:username, username, file(__file,username, username, username, username(username(file =file_user,user(__from_maybe_username.with_username.username_from_username.utils.hash__filenamefrom.__user.__request_file_project__<test_maybe.file =file_with__file_user_from_file =file_from <user =uni__date_token_from_path_file_token_filename_user_token_user_test_file_user_user_token_from_user.<<util_model
+ut_ut_hash_hash_user_file_project_fi_user_path_hash_user_user =user_file_from_hash_project_user_pathuser_user____file_file_from_b_b_path_file_hash
+<valid
+valid_valid_user_file_user_utils_from_user_user_validty_from_tokenrrkey_hash_hash_file_hash_key_repo_rr_user.repo_from_valid_rr_hashr_d<hash_user_user_from_to_db_fromr_token_username_validate_from_timrsrr<<token_from<from_token_tr<<from<hash<hash_hash_hash<hash_<timestamp_token_rr<<hash<t_hash_hash_hash_repo<e_hash_valid_token_repo_hash_hash_hash_hash_hash_hash_hash_ut_repo_hash_dir_hash_repo_repo_repo_repo_hash_hash_hash_hash_hashhash_hash_hash_hash_hash<hashhash_hash<hash<<hash_hash_hash<hash_hhashhashhash_hashhashhhash_hash_hashhash_hhash_valid<<<<ut_auth<<hash_hash_hash_hash_hash_<hash<<hash_valid_repo_hash<<<hash<hash_hash_hash<_hash_hashhash<hash.hash.hash_hash_hashhash_hash_hashhash_hash_hash_hashrhash_hash_hash_hash<hash<hash_hash_hash<hash<hash_from_hashhash_hashr_hash_hashhashrthashrhhash_hash<hashutils<hashhhash
+hashh_hashhhasht_hashhashhashhashhashhashuthash_hash_hash_hash_hash_hash_hash_reporepo<hash<<reporhash_hash
+hashhash_hashhashhashhto_hashhash_hashhash
+hashrhashhashbrepo_hash_h<hashuthash
+repoh_hashhashhashhhashhashhashfromhashfromhash_fromhash_hashmhash<_<U
+h
+hash<hashr
