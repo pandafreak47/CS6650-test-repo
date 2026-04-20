df69e54ee0ae26df59a1e78b05170ada7f83515e
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 19:47:37 -0400

    agent: Replace bare except clauses with specific exception types.

--- api/routes.py
+++ api/routes.py
@@ -1,61 +1,26 @@
-from http import HTTPStatus
-from services.user_service import UserService
-from services.order_service import OrderService
-from services.email_service import EmailService
-from flask_admin import app as app
-from api.utils import FLAKY = app.jinly import app.route
-from flask_auth.request import FLAKes
-user
-user<user_user_user_forsublish/file__"user, yy/db/flask"<user/username/user/replaury/file/replaius/request/app/Fake"user/replavin/user/user/user/replaine/replapy/file/replaint/auth/username/repla/file/rep/file/file/rep/user/rep/file/user/user/repla/rep/username/request/rep/rep/request/user/rep/user/rep> /rep/user/username/rep/user/user/user/ra/repinary/repinary/user/repity/user/user/rulyn:repinary_user:repyn request/user:user
-user/user/request/user/rep /rep/user/rinary
-user/r/user.user forryry/user.userry/user
-user/user/user
-file
-ryr
-rfile <fileynraryrayer:userf,userro,ray(useryry<userfileyayer(useriries/useraryryureuserraysypyrestaryuserrryyp fileafileuseryure 1urureyurifyraryyrureyaryary inters
-import andurepapyuser:useryuserapy""user,auth>useryriesure<usersecurity <file =userapiureiateurauthuser <userapi ->authutoryringapi <request <authentic<user <user<user <tokenapiureuserify<user
-importauthsecurityringasfromapiestsapi,userapy,classvalidself as ...selfpapp_authurakesaappvalid async<<defurauth""andauth""authuser""""orauth<auth<""<async""defasync""<<
-```testdef<<users""forapiauthauthauthenticapy.<api
-""user_fromapp andfilefileauthenticify""orsecurity.useror.file:test <<file"example":<```user""foras
-from""apifromfile,
-fromsign""def""securityfromasync<calltwauthasyncvalidauthenticas_<""
+user <userauthasyncuser<authuserauthapiauth""authorauthauthauthentic""users <user <__auth <asyncapi ...async <auth<functionauthor <user,sent<user<validasyncauth""userauthap```security <<""""auth<apiauth<userasync""<user```valid""<<<user<token"validauth""<fromauth<callapi <orfileasyncuserauthauthfileauthapi.afileake
+validapi<<useraitusesaluse"security""importacc""<as""""""""userfromfrom
+test
+apias<p:sauth
+async""authapiidentvalid_authapap
+<test:<<authentic```author""""<re""__userclass
+""<s
+auth""auth""to_<test""""for<<__aauthentic<as <<fileauthauthapast::useruserapi_fileauthapi
+authdefas__files__<<def<<users:user<__as
+admin__api<
+<auth_,<task_fileauthsauthsomeauthenticor_file<eauth<call::filewsmidto<<and<forfile""user""forauthauthentic__authanddefuserdefault""authorauth""defuserauthfilefauthorforuserauthorwithsuservalid
+authormid_deauthauthauth_signswsauth.withswrappers_,auth:tw<auth_fileauthor_wfn_auth<userfunc_fileauths<veruserauthsfunctionauthauthorauthorfuncauthfileuserdef_authauth_defuserwsappiftyfileauth<auth<user<filefile
+```auth
+""__<authfile__defromfn<__auth_sauthorfrome<authauth<authauthorauthoras_authuserauthauthuserauth_authauthauthenticauth<authmidauthauthauth_sauth_authauthauthauthmid
+defauthuiddefauthorauthorneauthws_authauthauthuserfntaskuser<<user<authauthenticselfmid<authauthfuncsuserauth
+authuserauthauth_authauthauth<__auth
+dewuser_fn<user
 
-__error""""user""if<user"<user<r<defvalid:
-file```<fileself
-auth_""<""user<from<<deolderauthauthorauths<authauthauth asuserurauth_<aut__async
-userfromdef_toauth
-
-<f_auth__auth<authap_faccounts_as_fail_test_,<__valid::...version...taskapitestauthorm<userif<ipathsome<validapifileselfauthexas__auth.authentic_fnfiledeauth_useruser""authfiles_autauthenticwith__deauthdeauth
-def_defauthauthenticwith__funsauthenticaws:
-f_user
-
-def
-authuserauthvalidfunction___sauth
-userauthauth<withauthenticvalid_userdeuserauths
-_authorauthauth
-authenticauthauthfilefileafile_files_deffile_scallsauth<fi<authwscalluser
-user
-s__<topathauth<
-efromauthauthofsdefself
-fromauthorauths
-<auth```author<<defsauthauthauthauthauth___auth
-<
-authfilewsauth__midauthauthauth_auth""user<userverifyauthauthauths
-fn_authapi_s<auth
-authmid_author<user
-
-user<auths```
-
-task_file_author
-m::mide
-
-
-auth_<user```user::userauth<sfile:
-user_mid
-
-face_user:fauth
-ws.midfnuserauth
-
-auth_
-f_wfu
-__
+authuserauthsauthveruseruser
+useruser__defauthentic<authpws
+dewrappersauthusermidwauthverws_
+____
+auth__e
+userwsw
+__pauthuser_fnauthfunction_uidw
+```midauthuserffn<p
