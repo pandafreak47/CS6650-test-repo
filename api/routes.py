fd1462ae22c0ecbbde48b0ce21405e00c7f6c4fb
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 00:08:59 -0400

    agent: Add type annotations to all function signatures.

--- api/routes.py
+++ api/routes.py
@@ -1,10 +1,6 @@
-apimysqldb.db.userapiapiimportdbtkfilesormws<mysqlormdb
-filesdb<mysql<<<fromfile <<asyncatwsimport
-<usera<
-t
-<user <database<<<db<<api_
-<user.api
-mywithdbmysqlpackagedb
-mfordbfrom
-fromdb
-from<<mata.apima<ma<db
+fileuser```userservice<username
+
+
+user user__user```user
+and
+user
