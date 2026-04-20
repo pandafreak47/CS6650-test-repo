4fdeccc0f46178d64173aca3c5ba8d34875b961b
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 12:47:47 -0400

    agent: Add an __all__ list that exports only the public API.

--- main.py
+++ main.py
@@ -1,64 +1,17 @@
-<output path>
-```py
-import auth
-@wrap
-def auth_user_auth_user_verify_token(authuser,token):
-    username = verify_token.split("Bearer ")[0] and "Bearer "
-    if auth.verify_password.verify(username, "Bearer.")user_username_password_user if auth.verify(username:verifytoken)
+user <useruseruseruseruseruser,useruser:useruseruseruseruseruser/useruseruser/user/user/userayer:user useryuseruseruseruseruseruseruseruseruseruseruseryuseruseruser
+useruseruseruseruseruuseruser,useruseruseruseruseryuseruseryuruseruseruseruseruser,user urure,user
+useruseruseruid <useruser
+useruseruser <useruser <useruser
+useryureuserayuseruseruserfile:useruseruseruseruseruseruser
+<userate
 user
-```
-```py
-fromauth import auth
-```
-@wrap
-fromuser
-
-import auth
-fromauth
-import
-user: import
-from auth
-from authuser_user, auth
-user
-import/auth
-from
-from authuser/from user/
-token
-user, auth
-username_password
-user
-fromtoken,user
-username_auth,auth
-fromuser
-from/user
-user_fromuser
-user_username/user/user/user/user/user/user
-
-from/user/auth/user
-user
-fromuser/auth
-from
-auth>user_user/user/user/user/user
-user
-user/userauth
-user
-user
-username/useruseruser
-username
-auth =user:user_user:user:user/user,user,user,user_user user
-user,user/user_user
-user
-user/user
-user_useruser/user/useruseruser)user/user/useruseruseruser
-user
-user,user/user.user <user_userauth,user:user anduser<user,useruseruseryusery<useryayeryturing:useryyuseryfile/user /useryate
-usery,useruseruseryusery andyuseruseruser user uate <useryuser ->tokenur.user,useruser
-valid:user<useruser
-useruserauthayerpath,userauthure
-auth
-user()file,user.user
-user
-authay
-useru <useraysayer
-user
-user asuser
+userauthuseruseruseruserassiateuseriateurappauthorauthauthauthsecurityuser,
+<authapiuser
+<useruser
+files
+```usersymuseruruseruleasyncapiasyncfileurap <anduserapiauthuserauth""authauth
+api """authsauthuserauthselfuser""user""""<useruserapiuser""user
+appiateur_userapyauthapp
+authauthenticauthenticaryapiateauthenticityiateateiate <""valid<<""asyncapiasyncuseruser""```<useracc""security""asyncuser""call""fromselfapi
+def
+responseauthauth""user.<authenticauthauthenticapivalid_authauthenticvalidify<user(apiapi...author...""de
