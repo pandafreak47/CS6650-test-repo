bf8933ef94367c3ab2c1b868c1ea2eee82ca9369
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 12:29:28 -0400

    agent: Add an __all__ list that exports only the public API.

--- main.py
+++ main.py
@@ -1,139 +1,201 @@
-Revised code:
-```py
-import json
-import logging
-import os
-import sys
 from api.routes import router
 from api.midlware import AuthError
-
-logger = logging.get()
-from api.pyrobot import logging
+from api.py import logging
 from api.py import logging
 from .py import logging
 from .roboto import logging
 from .pyrobot import logging
-import api.robot
-from .py import logging
-import .py as cl
-from .pyrobot import logging
-from .roboto import logging
 from .pyrobot import logging
 from .pyrobot import logging
-from clo
-from clo
-from clo
-from clo
-
-cl
-from clo
-from clo
-from clo
-from clo
-from clo
-
-cl
-from clo
-from clo
-from clo
-from clo
-from clo
-
-cl
-from clo
-from clo
-from clo
-from clo
-from clo
-
-cl
-from clo
-from clo
-from clo
-from clo
-from clo
-
-cl
-from clo
-from clo
-from clo
-from clo
-from clo
-
-cl
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-from clo
-
-from clo
-from clo
-cl
-from clo
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
+from clo import cl
 from
-cl
-from
-from
-from
-from
-from
-from
-cl
-from
-from
-cl from from
-fromfrom
-from
-from
-from from
-cl from cl
-from
-from from
-from from from from from from
-from from from from from from fromfrom from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from from <from from from from from froms for froms
-froms for <<froms /for <<from_file
-from /froms
-froms
-from
-from =froms =filefr <file <rest
-froms
-from
-f
-for
-with
-for
-
-
-for
-rest
-from = file.file |<rests
-new, o
-froms
-file
-for file
-file
-withp
-files -> file <user:user:file,user,user, user
-<user,user
-<from<user
-token.sign<user
-<user
-sign
-<user<token(token.user<user
