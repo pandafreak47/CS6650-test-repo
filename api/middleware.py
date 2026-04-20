32cb8b2959a680274e771d9b75dbe5657f474883
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 15:01:39 -0400

    agent: Add type annotations to all function signatures.

--- api/middleware.py
+++ api/middleware.py
@@ -7,144 +7,90 @@ from .models import User
 from .services import Service
 from .validators import Validator
 
-
 if TYPE_CHECKING:
-       User = User
-       Service = Service
+        User = User
+        Service = Service
 
 
 @overload
 def require_auth(fn: type, *args, **kwargs):
-       return fn(**kwargs)
-       return fn(*args, **kwargs)
+        return fn(*args, **kwargs)
+        return fn(*args, **kwargs)
 
 
 @overload
 def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, **kwargs)
-       return fn
+        return fn(*args, **kwargs)
+        return fn
 
 
 @overload
 def require_auth(fn: type, *args, **kwargs):
-       return fn(**kwargs)
-       return fn
+        return fn(*args, **kwargs)
+        return fn
 
 
 @overload
 def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, **kwargs)
-       return fn
+        return fn(*args, **kwargs)
+        return fn
 
 
 @overload
 def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, **kwargs)
-       return fn
+        return fn(*args, **kwargs)
+        return fn
 
 
 @overload
 def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, **kwargs)
-       return fn
+        return fn(*args, **kwargs)
+        return fn
 
 
 @overload
 def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, **kwargs)
-       return fn
-
-
-class AuthError(Exception):
-       status = HTTPStatus.UNAUTHORIZED
+        return fn(*args, **kwargs)
+        return fn
 
 
 @overload
 def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, current_user=None)
-       return fn(*args, current_user=None, **kwargs)
+        return fn(*args, current_user=None)
+        return fn(*args, current_user=None)
 
 
 @overload
-def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, current_user=None)
-       return fn(*args, current_user=None, **kwargs)
+def require_auth(fn:
+        fn
+fn:
+        current_user:current_fn
+        fn:current_user:
+        fncurrent:
+                current_user
+                username)
 
+            fn:username:current:
 
-@overload
-def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, current_user=None)
-       return fn(*args, current_user=None, **kwargs)
+<| current_user_username:current:current_user:current_user:current>
+current:current:current:username:current:current:username:current:current:current_user:current:username,current, current, username:current:username:current_current:current:current:current:username:current:current_username:current:user:current
+current:current:username:current:current:user:current>current
+current
+username</current username_currentusername
+current_current
+username:current_username_user:username
+username_user
 
-
-@overload
-def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, current_user=None)
-       return fn(*args, current_user=None, **kwargs)
-
-
-@overload
-def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, current_user=None)
-       return fn(*args, current_user=None, **kwargs)
-
-
-@overload
-def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, current_user=None)
-       return fn(*args, current_user=None, **kwargs)
-
-
-@overload
-def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, current_user=None)
-       return fn(*args, current_user=None, **kwargs)
-
-
-@overload
-def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, current_user=None)
-       return fn(*args, current_user=None, **kwargs)
-
-
-class UserAuthError(Exception):
-       status = HTTPStatus.BAD_REQUEST
-
-
-class ServiceError(Exception):
-       status = HTTPStatus.INTERNAL_SERVER_ERROR
-
-
-class InvalidCredentials(Exception):
-       status = HTTPStatus.UNAUTHORIZED
-
-
-class TokenExpired(Exception):
-       status = HTTPStatus.UNAUTHORIZED
-
-
-class InvalidToken(Exception):
-       status = HTTPStatus.UNAUTHORIZED
-
-
-class InvalidScope(Exception):
-       status = HTTPStatus.FORBIDDEN
-
-
-class UserNotFound(Exception):
-       status = HTTPStatus.NOT_FOUND
-
-
-@overload
-def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, current_user=None)
-       return fn(*args, current_user=None, **kwargs)
-
-
-@overload
-def require_auth(fn: type, *args, **kwargs):
-       return fn(*args, current_user=None)
-       return fn(*args, current_user=None, **kwargs)
-
-
-@over
+current
+current
+user,user,current
+user_user_username,username, user:user_username_username:user:user_username
+user:user:user:user:user:user,username:user, username:current, username:user:user</username:user(username:user(user:user.user_username.username #current:user:user,username:user,user <user_user,username
+user:user:username:user:user:user:user,u():user())file,username,userfile:userfilefile(username(userfile.user.username,username:user,user_user.user(web.user_file.current.user.user()user.username_file(user <username.user(__file <async <random_tokenice_user =user =usernameiate_user ->from_files<user__test_fromut_from_user_test
+<from::user_user_user::web_test_user_usernameutor<user_testentialuteuser ->__from_delay.file_user file_file<user.utute ->ut<file_ut_user_user_ut_file.file.with_useruser.<test_user
+<tim__user_```user
+valid_ut_randomut_prov_async.user,<with_if_test__fromutilsfromfrom<red.r__<tim_user
+from
+from_data__user.runuser<user<fromfromfrom_hash_account_hash_user_test_with_<tw_random_rate_<rate_hash_usernamefrom_e_d<<tim_email<rep<valid_to_fromfrom_fromjson_user_#touser_from_fromfrom<testu#with<user<fromhashe<<<<<with
+<from<<<test
+<_rep_to_hash<<h<_<hash_hash<__<<ut_hash_file<<shrepo_hash_file_repo_repo_repo_repo_root
