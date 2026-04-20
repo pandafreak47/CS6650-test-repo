57af4070cecf0718ef243fe54803a50c7bb3e3f5
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 00:22:36 -0400

    agent: Add type annotations to all function signatures.

--- utils/auth.py
+++ utils/auth.py
@@ -1,3 +1,8 @@
+<file path="db/user_repo.py">
+<file path="models/user.py">
+<file path="views/user_registration.py">
+<file path="views/user_profile.py">
+<file path="models/user.py">
 ```python
 from db.user_repo import UserRepo
 from utils.validators import validate_username
@@ -6,176 +11,60 @@ from flask import request, render_template, session, flash
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_utils import UUIDType
+from flask_admin import Admin, AdminForm, expose
+from flask_admin.contrib.sqla import SQLAlchemy
+from utils.validators import validate_username
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, validators
-from flask_wtf.csrf import CSRFProtection
-from flask_admin import Admin, AdminForm, expose
+from wtfform_admin_twee import WTFFormAdminTweet, WTFFormAdminUser
 
-from flask_admin_tweets import TweetAdmin
-from flask_admin_tweets import TweetForm
-from flask_admin_tweets.fields import TweetField
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin_twee import AdminTweetAdmin
+from flask_admin_twee import AdminTweetForm
+from flask_admin_twee import AdminUserAdmin
 
-from flask_admin_tweets.views import TweetsView
+from flask_admin_twee import AdminTweetCreateView, AdminTweetUpdateView, AdminTweetDeleteView, AdminTweetListView, AdminTweetCreateForm, AdminTweetUpdateForm, AdminTweetDeleteForm, AdminTweetListViewForm
 
-from flask_admin_tweets.models import Tweets
-from flask_admin_tweets.models.fields import UserField
-from flask_admin_tweets.models.fields import TweetField
-from flask_admin_tweets.models.fields import TweetFormField
-from flask_admin_tweets.models.fields import TweetLinkField
-from flask_admin_tweets.models.fields import TweetLinkListField
+from flask_admin_twee import AdminTweetCreateView, AdminTweetUpdateView, AdminTweetDeleteView, AdminTweetListView, AdminTweetCreateForm, AdminTweetUpdateForm, AdminTweetDeleteForm, AdminTweetListViewForm
 
-from flask_admin_tweets.views import TweetsView
+from flask_admin_twee import AdminTweetCreateView, AdminTweetUpdateView, AdminTweetDeleteView, AdminTweetListView, AdminTweetCreateForm, AdminTweetUpdateForm, AdminTweetDeleteForm, AdminTweetListViewForm
 
-from flask_admin_tweets.admin import AdminTweetsAdmin
 
-from flask_admin_tweets import AdminTweetsAdmin
+from flask_admin_twee import AdminTweetCreateView, AdminTweetUpdateView, AdminTweetDeleteView, AdminTweetListView, AdminTweetCreateForm, AdminTweetUpdateForm, AdminTweetDeleteForm, AdminTweetListViewForm
 
-from flask_admin_tweets.views import TweetCreateView
-from flask_admin_tweets.views import TweetUpdateView
-from flask_admin_tweets.views import TweetDeleteView
+from flask_admin_twee import AdminTweetCreateView, AdminTweetUpdateView, AdminTweetDeleteView, AdminTweetListView, AdminTweetCreateForm, AdminTweetUpdateForm, AdminTweetDeleteForm, AdminTweetListViewForm
 
-from flask_admin_tweets.views import TweetListView
 
-from flask_admin_tweets.fields import TweetField
-from flask_admin_tweets.fields import TweetLinkField
-from flask_admin_tweets.fields import TweetLinkListField
-
-from flask_admin_tweets.fields import TweetImageField
-from flask_admin_tweets.fields import TweetLinkImageField
-
-from flask_admin_tweets.fields import TweetTagField
-from flask_admin_tweets.fields import TweetTagImageField
-
-from flask_admin_tweets.forms import TweetCreateForm
-from flask_admin_tweets.forms import TweetUpdateForm
-from flask_admin_tweets.forms import TweetDeleteForm
-
-from flask_admin_tweets.forms import TweetListViewForm
+from flask_admin_twee import AdminTweetCreateView, AdminTweetUpdateView, AdminTweetDeleteView, AdminTweetListView, AdminTweetCreateForm, AdminTweetUpdateForm, AdminTweetDeleteForm, AdminTweetListViewForm
 
 import os
 import json
 import datetime
 
 
-@login_manager
 class User(User):
      
-     def is_active_user(self, userid):
-         return UserRepo().is_active(userid)
+      def is_active_user(self, userid):
+           return UserRepo().is_active(userid)
 
-     def is_admin(self, userid):
-          return UserRepo().is_admin(userid)
+      def is_admin_user(self):
+           return UserRepo().is_admin(self.id)
 
-     
-     def __repr__(self):
-          return "<{}>".format(self.username)
+class UserCreateView(AdminTweetCreateForm):
 
-     def __repr__(self):
-         
-     def __repr__ = self
-         
-         return User.username.lower()
-
-
-class UserCreateView:
-         UserRepo
-
-         self:
-
-class Tweet:json
-username
-class: Tweet
-class.txt)
-username: Tweet:json
-
-self:
-class:user
-
-tweet:
-Tweet.json
-
-username:json
-User
-from
-username
-username:json: Tweet:json
-T
-username
-T
-
-json
-
-json:username
-
-
-T
-T
-T
-
-username,json:
-
-
-
-T
-json:T
-T
-
-Tjson
-
-T
-
-T
-T
-json
-json
-json
-T
-T
-
-json
-T
-
-T
-json
-T
-json
-T
-json
-json
-T
-username
-user
-
-TT
-T
-T
-json
-T
-
-T
-T
-T,T
-T <row
-T
-T
-T
-T
-json
-T, TT
-json:username
-T
-TT
-T
-T
-Tries
-TT
-Treekries
-TT
-T files
-T
-TionsrTtries
-withtriesy
-TTyringtries for TT
+      def clean(self):
+            
+               self.username = UsernameField()
+             email = Email = True
+             Password = 3 > 35
+             Twitter.id = 45678
+            3:json: 7 = 8:3> Username:3:3:email:username:
+            password =6:3,7:47:username.3:8:username = Twitter.id5:6:5:1:2: 5:email5:3:7:username:3framer_unicity:10:email@username:1:3:username:3 = 3 =3: 3 3, 1, 3(username>3)
+3)
+user_3
+username:3<username
+username:3>3,5@1<355`username__3f<3<user>
+3,username #3<username
+3
+3
+5 33 35655_3<3<3 <user.3.Username(35>3.user.username.3`3:3:username.3_username:<username:username(3e<<regular)usernameual>user>username.user>`<valid.user.3euf <file <file_fileepecketmate_3<file <<file>.username<file<file\.user._<user(domain_maybe_mails<mail(word<<sent
