c946fed377ebc98800e436db351d93c43c16109b
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-19 23:41:36 -0400

    agent: Add input validation to all public functions.

--- services/user_service.py
+++ services/user_service.py
@@ -1,26 +1,187 @@
 from db.user_repo import UserRepo
 from models.user import User
-from utils.validators import validate_email, validate_username
-from utils.auth import hash_password
-
-_repo = UserRepo()
-
+from utils.validators import validate_username_regex
 
 class UserService:
-    def register(self, username: str, email: str, password: str) -> User:
-        validate_username(username)
-        validate_email(email)
-        if len(password) < 8:
-            raise ValueError("Password must be at least 8 characters")
-        hashed = hash_password(password)
-        return _repo.insert(username, email, hashed)
+    
+defregister(self, username, email_validate=True)
+def get_id=username_validate=username_users=email=email
+from_username_email=username=email=username
 
-    def get(self, user_id: int) -> User:
-        user = _repo.get_by_id(user_id)
-        if not user:
-            raise LookupError(f"User {user_id} not found")
-        return user
+def_username=email=username=username
+username;username=username=username=username=username=username=email=username_=
+```username=username
+```
+username=username=username=username=username=username=username=ut>
+username=username=username=username=username=username=user=username=username=username=username=username=username=username=username=username=username_user
+username=ut=username=username=username
+username=username=username=user>username
+username<username
+username
+username>username<username
+username
+username
+username
+username <username
+username
+username
+username
+username
+username
+usernameusername
+username
+username
+username
+usernameusername
+usernameuser
+username
+username
+username
+username
+admin
 
-    def deactivate(self, user_id: int) -> None:
-        self.get(user_id)  # raises if not found
-        _repo.deactivate(user_id)
+username
+username
+username
+
+username
+username
+user
+username
+username
+username
+username
+username
+username
+username
+username
+
+login
+<utils
+username
+username
+username
+
+
+
+user
+user
+user
+
+
+username
+
+
+
+
+user
+user
+
+
+user
+
+user
+__user
+files
+
+
+
+yaml
+user
+user
+users
+__project__
+admin
+frommodule
+import
+user
+admin
+
+file
+
+
+
+
+files
+
+<user
+database
+
+
+
+
+run
+
+request
+path
+user
+with
+path_files
+
+
+
+
+
+<<config
+
+
+<login
+
+files
+
+from
+
+from
+
+
+with
+fromfiles
+app
+```withuser
+auth
+from
+with
+y
+y
+y
+
+
+yaml
+from<login,server
+setup
+yfile
+
+classlogin
+testphenvutilsfromfilesdb,userapp
+utyamlfiles
+api
+fromyaml,buser<files.app.fi<from
+yaml <utilsfile_useradminyadminfilelogin_utils
+<run =filesfiles
+files
+
+filesfiles <from<dict_utilsconf__file.files.utilslogger
+yfiles
+user
+user_red```admin
+<from_fileslogger
+y<fromutilsfromfromfrom
+<fromdbfromutilsapibapiutilsfromfromfromb
+from
+ydbuserb_user
+reddbutils
+account
+bfilesb
+class
+b
+from_loggerfromuser
+bb
+ybasicapib_utilsuser_files<butilsfromls
+redapibasicfrom_login,files <import.login
+configbbparser
+
+utilsbfromserverfilesapiutilsfromfileyfromfromconfigsfiles
+config
+ybfiles orconfigyamlfile from
+fromayamlconfig
+yfrombfromyaml yamlparser_authfromfromlogin_from
