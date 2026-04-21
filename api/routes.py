8d733264fa21d8ba48768e5aa0a3f8812eebf0f0
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-21 00:49:40 -0400

    agent: Replace bare except clauses with specific exception types.

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,3 @@
-email,fileger(id>user_only, ...from ...user,file, id >profile<profile <from file >user file
-users.user from email <email<token_user
-user <user <user<<user >user <user_file <file__token <user,user.token <pick_user__sent <file_tourent_success ->file_model(user<user_email_file
-user <user_login <file<file_file <user <graphirs <graph_<users_valid_email__<email__usersums <<user_profile <filee_sign__file
-project.user.profile.test..api_<user_user""user <create or ->db_profile_user_user
+```userficentats ...databaseftfersing
+``````<dbs``````<and__```<user``````````<````````````<withification`,```<file<``````<```<sign_file<domainzingentialities`,```<```userfile``````users`, ``````````<````````````user`, `````````or`,<<```<filering<filesheet file `forionersplitatory```...orders`<user`,test/`<call <file
+<``````file ```<```<and ```file``````````underheetheet`user`function`,allidingusersingssser<``````__
