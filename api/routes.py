8ffe19d5641007715376ac5ab991868d62df30e3
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 23:15:35 -0400

    agent: Replace bare except clauses with specific exception types.

--- api/routes.py
+++ api/routes.py
@@ -1,30 +1,117 @@
-```
-from http import HTTPStatus
-from api.middleware import require_auth, AuthError
-from services.user_service import UserService
-from services.order_service import OrderService
-from services.email_service import EmailService
+fapi_w_api
+mmidauthauth_mid_wrapper_auth_authauth_auth_w_user_sapi_wfnfn_authauthauth<auths_wrap_auth_auths_w_w_r
+w
+_s_auth_wrapper_all,_wrap_n_auth_api_auth_f_auth_r_auth_api_user_r_auth_w_n_n__w_r
 
-_users = UserService()
-_orders = OrderService()
-_emails = EmailService()
-```
-```
-<|file|>
-```
-<|
-from api.midddict import AuthError, Auth, HTTPStatus
-```
-```
-from services.auth.UserService import User, AuthError
-Email, Admin, Email
-from services.User from file from api.Auth, email, password.Email from File, Email, _user, self.Email, api, Email from, Auth, Email, Admin, Auth, Email, Auth,Auth, Email, Auth, Email,Auth,Email,File,Email,Auth, admin, Auth, Auth, Email, Email, Auth, Auth,Email, __file, file/User,Auth, user, Email, Email, Email, /file,Auth, user, user, Admin, Email, Auth, Auth, file, Auth, _Password, _user, Auth, Auth, Email, Auth, Auth, Auth, Auth, Auth,Auth, Auth, Auth, Email, Email, Auth, user, Auth, Auth, Auth, Auth, Email, Email, Auth, Auth, Auth, Email, Auth, Auth, _user, Auth, file, Auth, Auth, Auth, Auth, Auth, Auth, Auth,AuthAuth, Auth, AuthAuth
-Auth, Auth, Auth, file, Auth, Auth, AuthAuth, user, Auth,AuthAuth, Auth, tokenAuth, user, Auth, Auth, fileAuth,AuthAuth,Auth <file, fileAuthAuth <file
-file(file()fileAuth file Auth, AuthAuth, AuthAuthAuthAuthAuthAuthAuthAuth,AuthAuth <file <file <file <api.UserAuthAuthAuthAuthAuthAuth> <user <Auth <<file ><user <file <user <fileAuthAuthAuth import files '''user <auth <user_session_async <importapy <users import <user <userauthUser import <userauthenvapyuser_authauths import:user_file,auth <fileakesapvalid as ...requestauth_asyncauthserified""token""auth:loginauth <security =<apiuser <authauth<api:auth <author""""author <valid""authauthtokenuseruser""file""<token<from <validauthauth<auth_userappauthauthenticuser_tokensecuritysdata_userify <userails.security,auth""<app""<userapi<security.from""api""<from""userauthauth""test""""valid""auth""_""author""<auth_auth...valid_error[:error ...from_authenticauth<valid<async_authentic::adminuser_authenticauth_auth_auth_call_auth""authentic""import importuvserifier<userauthsauth_user_api_authauthalauth from<authentic<securityauth_author_authenticauth:<user_author_<author_mockapi_authmockauth<auth
-authauthauth""auth""auth_authauthauthauthtyauthmiduid""_authenticmidauthauthapiauth<userorms""mid""""_author_token__valid_auth_authapi_authapiauthauth_auth_user_mid<fun""api_author_authenticauth_auth_auth_auth_author_auth""wrapper_authauthauth<func_verify:authapi_auth
-auth.auth<authenticfn_apirauth""authapi_authauth<authif_authauthauth_authmidauthauthauth_authormidif<authenticif
-auth_auth_authenticif_fromauthauthauthmid...authsauthauthauthauthauthmid<authauth_auth_authauthauthmidauthmid
-auth_auth""auth_authmidauthauthauthauthauthauthmidmidauthmidauthauthauthauth_authauth
-auth_authauthauth_authselfauthauthauthauthauthauthauth
-auth<api<<auth""auth_auth_authauth_auth_authauthauthauthauthauthauthauth
-auth_auth__authmidauthauthwrap_auth_fn_mid_auth_mid_authmidauth_auth_authauth<auth_authentic_auth...verify_auth_auth_fnauth_funcauth""mid_authauthauthauthmid<auth_authauth_authauthauthauthauthauthauth_apimidauth_authapiuser_auth_authauthwmidwauthapi<auth<authauthauthauthauthfnauthauthauthapi_apiauthauth_auth<<midauthauthfnfnmid_
+_r_to_f_wrap_auth_r_wrap_auth_ifnr_wrap_wrap_r_nif_tor(user_w_wrap_e_,nn___fn__wrap_(w_wrap_nf_wrappernw_r_nr_n_n_,n_wrap_
+n.tonwrap_wrapn_wrapfnfn_user_user_wrapn,
+r:r,n<wrap_tor_r_<user__r__user_user_user_r_wrap
+<
+
+
+
+wrap
+wrap_ewrap_r_r_r_user_user:user_from_wrap:wraprwrapr_wrapnfn_n_iffn_fn_wrap(wrap_wrap(wrap_r_for_fn_wrap__wrap_fn_r_rauths
+nr__midr
+wrap
+verify
+
+n_verify(wauthwrapwr:wrap:user,wrapr,r
+""r
+wrap_
+nwrap
+verify
+""
+r
+""
+w
+r_fnfn_fi<r""
+in"for""n
+n_fn
+n
+nfn
+wrapfn
+wrapfnffn_f_f
+fnnwrap
+wtofnnndwrapwrap_inwrapfn
+wwrap
+r_fnwnwrap_wrap_wrapwwwrapwrapw_wwrapwwrap__fnnfn_fn_fn_wrap_wrap<wrap_r__user_wraprw__userwrap_wrapfn
+wrap_w_wrapper<w
+
+
+
+wrapw
+if_fnwnfn,fnf
+wrap_fn
+
+n
+fn
+fn_fromn,
+
+fn
+wrapper
+w
+
+
+fn
+fnfn_
+wrap_nfnnin
+fn_wrapper
+nw
+r_wrapwrap_wrappernn
+wrap
+
+nin
+fn
+
+
+
+
+in_mr_
+
+
+_wrapper
+
+
+
+
+
+fi,wrap
+
+wrap
+fn_n_fn
+n_n_ndfn_n
+
+wrap_w_fnfn_fn,wrap
+fn
+
+
+
+
+fn
+fn_fn
+fn_wrap_f_wrap,_n_f_n_fn
+_
+fn_
+_wrap_
+n_from_
+n
+
+
+
+fn_fnn_nfn_fn
+,fnfn
+fn
+fn_fnnfnnfn_fn_nndnfnn,
+fn_fnfn_fn_fnfn_fn_fn_nfnnfn_fnnfn
+fnnnnfnfn
+fn
+fnnfn_
+fnnfn_fnfnfnfn
+
+nffnfnfn_fn
+
+fn
+
+
+wrapper
