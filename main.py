51ca77220a79d444376c197876d162caad27bc42
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 23:18:09 -0400

    agent: Add an __all__ list that exports only the public API.

--- main.py
+++ main.py
@@ -7,52 +7,27 @@ from api.middleware import AuthError
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s | %(message)s")
 logger = logging.getLogger(__name__)
 
-
 class Handler(BaseHTTPRequestHandler):
-       def _dispatch(self, method: str):
-           body = {}
-           if self.headers.get("Content-Length"):
-               body = json.loads(self.rfile.read(int(self.headers["Content-Length"])))
+        def _dispatch(self, method: str):
+            body = {}
+            if self.headers.get("Content-Length"):
+                body = json.loads(self.rfile.read(int(self.headers["Content-Length"])))
 
-           token = self.headers.get("Authorization", "")
-           handler = router.get(f"{method} {self.path}")
-           if handler is None:
-               self._respond(HTTPStatus.NOT_FOUND, {"error": "Not Found"})
-           else:
-               try:
-                   status, data = json.loads(body)
-               except:
-                 logger = self
- from __all__.py", " from file" "auth import "from " from api.User from __file" fromauth <file__auth fromfile "file " from file "Auth <file __auth file from file" "Auth <file Auth", file __file from fileAuth from __file from __file __ Auth, __ from auth Auth, from file Auth Auth " from file fromfile from __file,Auth from file from Auth, Auth from file, Auth, from__user__ from Auth,Auth fromfile from from /__Auth, Auth from file from __file, Auth from file from __from Auth, from __ fromfile from __file from __file from Auth from Auth from __file from __from Auth from Auth from <from __file from __Auth
-from file /Auth
-from /Auth from /Auth Authfile
-from,Auth from /file fromAuth fromfile, fromAuth from file <from Authfile
-from
-from file
-from file, Auth from file from from file from <from file from from Auth from fileAuth from file from from from file
-from fileAuth from fromfile from from fromFile from from file fromAuth from __from <file Auth from Auth
-fromAuth file
+            token = self.headers.get("Authorization", "")
+            handler = router.get(f"{method} {self.path}")
+            if handler is None:
+                self._respond(HTTPStatus.NOT_FOUND, {"error": "Not Found"})
+            else:
+                try:
+                    status, data = json.loads(body)
+                except:
+                    logger = self
 
-<fileAuth, file
-file ->
- <from fromfile /file>file <from file<from<File
-file/file,file <<file /file
-file >
-file
-file /File
-<
-<
-file
-file,file,file, file and<<create <session -> ->filefile
-api
-<user/session
-<<users/file <__file <file >api<<
-files
-files
-user
-UserAuth <user
-files < < < <User
-<auth
-file -> = <userUser
-<<requests
-<import ->auth,userapi,file
+                from __all__.py", " from __file__" "auth import "from " from __file__ from Auth", " from auth __file__ from file "fromauth <file__auth fromfile "file " from file "Auth <file __auth file from file" "Auth <file Auth", " from file "from __file__ from " file " from Auth, from file " __Auth" " from file from file " " from file __Auth " __ from file from__user__ from "from Auth "__user from __user__ "__file " from __file from __user " " __user from __file __user " from __user " from file " fromfile " from __file " from __ from file " fromfile from file from file " " from from __user from __user fromfile from from file "from __user " from " fromfile " from file " from __user fromfile " from user " from " from " from "file "__user from " "file " " from " from file " from " __from " from __user " fromfile " fromfile " __user "
+                    " from "user " __user " from " " from " fromfile " from " from __user "__ from " fromfile " from "from "from "from " from "user from " " " __user " from " from " from__ fromuser " " from "from from " from " from " from fromfrom from " from __user " " from __ from __ from __ user__ from> " from __ from " from> from__user_user.user "user from __user from__ from __ from__user
+
+ from __ from__user__from from from__ from __from 1<user__user from__
+ from__ from f. 
+
+__from nd
+e
