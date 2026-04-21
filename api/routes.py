9160e61fd6184613412d1323b52e2cdb7170aea1
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 22:45:40 -0400

    agent: Replace bare except clauses with specific exception types.

--- api/routes.py
+++ api/routes.py
@@ -1,2 +1,5 @@
-__def""__fromificational
-```""_user__...a____<api...import""with ...test""```from ```frome""fromfrom ...""""""import...<e"""""""""r""authenticuserignasync """session """mysql_ifaerror""
+import""""<for__token.iforapi._user_for_authenticeauthentic<withauth...__
+__api
+securityals,pass""...""forauthuser""""""""__app""from....from
+sfrom""withuser
+s""""""userauth""user""authvalid ...fromvalid""""validate::__re__""__user
