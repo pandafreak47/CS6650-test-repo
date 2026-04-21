7a5649833d9012828217cabb6aa4abe46536fd39
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 22:41:12 -0400

    agent: Add an __all__ list that exports only the public API.

--- services/user_service.py
+++ services/user_service.py
@@ -1,6 +1,114 @@
-with_conn<conn_s<< _db __yconn_db
-__testconnm __...connect...
-____f__fromconnect<_th__useruser
-__file #<connect
+sql ...
+db
+user__database_connectionconnection_conndbuser_dbconn
+__db
+user
+user |db |test |user_user
+user |_e_test_db_conn_db:
 
+<test |file|test_
+check.db | |
+ty"" |
+____
+__users()data____
+""user_```<db.db |dbuser ... ... |_db_db__user_useruseruser.sql
+<__user
+
+""conn
+connect ...from __...""_db_conn
+__connconn ||file""user""__user.
+ ...ty""test""conn""database |user
+exec__conn_user
+def_con.data
+
+db
+...__ |user_test ... | |_con_test(fdb_conn__
+conn()ty_user_db_connconn_connection_ty_tydbtydb__yconn_db_database_t_<user_conndb<f___tyconnconn __f_f
+___<""th_user
+
+<user___connection
+ |conndbimport_
+connect
+conn
+conn
+
+runuser
+...
+def___
+user
+
+ |
+db_id |
+connect
+m_connect_conn ...db._user <
+connconn__connect ...connect ...__conn
+connect_tuser___userty
+
+path__connection<f
+_conn_
+
+user
+userdbf
+
+db
+conth_from<""__connect_rep__<__
+fdb._connectth__user_
+rexm_
+connconnuserconn__conpy |
+user_db:f_______r._user ...fconnect...user___database_connect______conuseruseruseruserrepuser_rep...user_fuser_f |
+__userf
+connconnectuser<db<userrepconntyuser<...db<connuser_
+r__<...connfrom
+
+usersdbgeth...db
+withuser
+iduserdbfcon__f...
+__f""f_,user_idfdb:user""""
+f_f__<__get_fdb""""connect__fdb_s..."yconn__idf_dbidself__conn_...dbgetiduser_f___conn_fe_..."get__user_dbgetgetconnfconn_
+fe___db_get__fuser_euser
+__get_self_fgetfget_getjsonconnusergetgetr
+get_con_get...conn_congetnb_connget_rget__get_rep_
+iddbidf_get
+get_user
+
+self
+connect_get_
+connectionconndb__conn_conn...f
+getget...conn_rdb_idfdb
+y
+for_
+selff_conn_db
+_he_rep_
+_connrep_conn___get_f____conn_f___tyself_f_getf_f
+get_
+f__self_get_get_conn_th_
 __
+____yfe...yrepf__get_connconself_self_ty_ydbdb__get_reptybf_fdbf_
+self_f_
+..._self
+
+rep
+_get___getfeget__get____dbdb_get __feffget
+rep_rep
+getconn
+
+get_dbuserget__fget
+_get_
+conn
+iddb__self__get
+___f__getgetget_fget__fgetget_get_f__f_
+get_____getget_getf_getfgetget:
+__dbrep___getfgetselfdb___get_getget_dbfidgetgetfe__f_db_get.fconnf__f__get_get
+get_get____getgetUser(f_getselfself(_(get(get(get(__get_f(get(self__user____getfget__f_fselffget_f__ffgetgetf__Fgetselfget
+Fselfself
+FGetGet(get(Ffget(f(get...fself
+get:fget(self(get_get_f(fff
+get_f_get_fself_fself__f(selffgetself(dbf
+__self(selfself_get
+fget
+self
+get
+get
+ffself
+__getself_f_selffe__getgetfselfself
+self__ffselfgetself
