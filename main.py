a8a2dc3b610e0ce78438af11ba07dc88d039a346
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-20 21:52:16 -0400

    agent: Add an __all__ list that exports only the public API.

--- main.py
+++ main.py
@@ -25,130 +25,182 @@ parser.add_argument('password', type=str,
 class User(Resource):
      def __init__(self):
          self.db = SQLAlchemy()
-with app.app.app.app:user.py
+with app.app.app.app.app:user.py
 ```python
 ```
-from flask_restful
-from User
-```_valid""""""""""
-"""
-```valid```
-from""<valid""""""""""""""""""""
-""<valid"""""""""""""
-""valid""```
-""valid""`valid`""<""""""""valid""""""""valid"""<valid"""""""""""""""""```""user"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""<user`) to""""""""""""""""""""""
-```""user""""""""""""""""<user`) to"""""""""""""""<valid""""""""""""""""""""""
-""""""""""""""""""""
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-"user""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-"user"user"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-```""user"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-}
-```
-<user>
-```
-""<assistant>```
-
+from <user>
 ```
 ```
 ```
+```
+```
+```
+```
+```
+user_service
+user.py
+user
+```
+```
+```
+user
+```
+user```
+```
+```user
 user.py
 ```
+
+user
+```
+user.py
+user
 ```
 ```
 ```
 ```
-```
-```
-```
-```
-```
-fromuser_assistant
 ```
 ```
 user
 ```
 ```
-<assistant
+```
+```
+user_assistant
+```
+```user.py
 user.py
-user```
+user.pyuser_user
 ```
 ```
-```
-```
+user<user.py
+user
+user
+<```
 
 ```
-``````
-```user`
-``````
+user=user
+user>
 ```
 ```
 ```
 ```
-```
-""``````
-```
-user````````
-
-```
-```
-```
-```
-```user`
-
-```
-``````
-```<user>
-```
-```
-```
-```
-```
-```
-
-```
-```
-```
-```
-```
-```
-```
-```
-```
-```
-```<```
-```
-```
-```
-`
-<
-<
-<
-<
-
-<
-```
-`
-```
+user
+<user_user_ass
+```user
+user>
 <user
+<user>
 ```
-```
-```
+user/user
+<user
+<user
+<useruser>user>
+<user
+<user
+<user
+<
+
 <user
 
+user
+user
+<user
+user
+useruser
+user
+<user
+<user
+<user<user
+
+user<useruseruser
+user<user<user
+user
+<user
+user
+user
+user
+user
+user
+user
+userpy
+user
+user
+user <user
+user
+user
+user
+user
+user
+useruser<useruser<useruser<user
+user <backend
+user <user
+<user <user
+user
+<user
+user
+user
+#useruser
+<user <tokenpassypass
+user
+
+<user
+user
+userpassr/user
+file
+file
+file
+
+user_root = <<
+<file
+<file
+
+files
+file
+<file
+<filecase
+<files
+<file
+<file
+<response
+
+<
 
 
 
+<
+<file
 <<
-```
-<<<<<<<<app < <<<db
-<<<<
-<<
-<api
+<<pass
+<<file <files <<<<<<<<<<json <<distr<app <<<<<<<<<<ser <class<<jsonjson<<class_signpjson
+<<json <<server_<<test_security <<<<server<<<<<<<<<<<<<<<<<<user_server <user <<<<<<<<<<<<<app
+<<<<<response_<<<<user.<<<<<<<<json <<<<<<pip_parserp<<<<parse<<<<<
+<<<<pip<json
+<<<
+prote <dis<<<<parse<<<<<json<json<<protefs<<<test<<<<<<<<<<<<<<<<<<<<
+f
 
-secret
+<
+<<<
+<< <
+test
+app
 
-<<
+<<<
+<apijson
+from<json<json
+fu<fromapi
+<<<<<<<<
+
+route""from <app
+pipapi<f
+pip<pro<json<pip <<<<<<<<<<<
+<<<from fromparsefort_<ser
+pip
+from<<run from
+from
+user_valid<parse<from_<db<path_pipparse<<de from
+valid and
+<pip<<<<<<dis
+valid
+res
