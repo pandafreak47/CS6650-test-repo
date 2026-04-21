384639b1af7ccf3d742ec437750f9c2431f86117
pandafreak47 <71993994+pandafreak47@users.noreply.github.com>
2026-04-21 01:53:09 -0400

    agent: Add type annotations to all function signatures.

--- api/middleware.py
+++ api/middleware.py
@@ -1,73 +1,69 @@
-from werkzeug.exceptions import BadMethodCallException, Conflict, Forbidden, InternalServerError, NotFound, ServerError, ValidationError
-
-
-@app.errorhandler(http.error.HTTPException)
-def handle_http_ex(ex):
-    return httpperr(ex)
-
-
 class BaseConverter(BaseConverter):
-     def __call__(self, value):
-         try:
-             return super().__call__(value)
-         except BadMethodCallException:
-             raise BadMethodCallException("Method not allowed") from ex
-
+      def __call__(self, value):
+          try:
+              return super().__call__(value)
+          except BadMethodCallException:
+              raise BadMethodCallException("Method not allowed") from ex
 
 class BaseConverterException(BaseConverterException):
-     def __init__(self, message: str = None, *args, **kwargs):
-         super().__init__(message, *args, **kwargs)
+      def __init__(self, message: str = None, *args, **kwargs):
+          super().__init__(message, *args, **kwargs)
 
-
-@app.errorhandler(http.server.ExceptHandler)
-def handle_http_ex(ex):
-     return httpperr(ex)
-
-
-class BaseConverter(BaseConverter):
-     def __call__(self, value):
-         try:
-             return super().__call__(value)
-         except BadMethodCallException:
-             raise BadMethodCallException("Method not allowed") from ex
-
-
-class BaseConverterException(BaseConverterException):
-     def __init__(self, message: str = None, *args, **kwargs):
-         super().__init__(message, *args, **kwargs)
+      def __str__(self):
+          return f"{self.__class__.__name__} ({str(self)})"
 
 
 class HttpException(HttpException, werkzeug.exceptions.Abort):
-     def __init__(self, message: str = None, *args, **kwargs):
-         super().__init__(message, *args, **kwargs)
+      def __init__(self, message: str = None, *args, **kwargs):
+          super().__init__(message, *args, **kwargs)
 
+      def __str__(self):
+          return f"{self.__class__.__name__} ({str(self)})"
 
 class HttpExceptionException(HttpExceptionException, werkzeug.exceptions.Abort):
-     def __init__(self, message: str = None, *args, **kwargs):
-         super().__init__(message, *args, **kwargs)
+      def __init__(self, message: str = None, *args, **kwargs):
+          super().__init__(message, *args, **kwargs)
 
+      def __str__(self):
+          return f"{self.__class__.__name__} ({str(self)})"
 
 class WerkzeugException(WerkzeugException, werkzeug.exceptions.Abort):
-     def __init__(self, message: str = None, *args, **kwargs):
-         super().__init__(message, *args, **kwargs)
+      def __init__(self, message: str = None, *args, **kwargs):
+          super().__init__(message, *args, **kwargs)
 
+      def __str__(self):
+          return f"{self.__class__.__name__} ({str(self)})"
 
 class InvaliadException(Invaliad, werkzeug.exceptions.Abort):
-     def __init__(self, message: str = None, *args, **kwargs):
-         super().__init__(message, *args, **kwargs)
+      def __init__(self, message: str = None, *args, **kwargs):
+          super().__init__(message, *args, **kwargs)
+
+      def __str__(self):
+          return f"{self.__class__.__name__} ({str(self)})"
 
 
 class NotFoundException(NotFound, werkzeug.exceptions.Abort):
-     def __init__(self, message: str = None, *args, **kwargs) -> werkzeug.exceptions.Abort:
-         super().__init__(self, message)
-         self.message:str = message)
+      def __init__(self, message: str = None, *args, **kwargs):
+          super().__init__(message, *args, **kwargs)
+
+      def __str__(self):
+          return f"{self.__class__.__name__} ({str(self)})"
 
 
-class ValidationError(ValidationError, werkzeug.exceptions.verify(werkzeug.py:message: Invalid):
-     werkzeug.verify werkzeug.py:
-verify:py:message: Werk:werk.py:message:werk.yaml:message:message: Werk:valid:
-:werk: Werkie: werk.message: Valid:message:werkie:
-:verify:message:y:message:message: Werk:verify, Werk:werk.py:message:message:werk:message:message:message:message:werk:message, message:message:message:werk:message:message:message:werk:message:message:message:message:werk.message:message:message:message:message:message:werk:message:message:message:message:message:message:message:message:verify:message:messageiveyake
-message
-message:verifyyeyer_message:message:werkyfileyymypyfileyyerfileymessageyapy:message
-messageyify:message:messageyeyeye.message:messageyrix.message.messageyymymymymyymymiteymifyymessageyym.message:yamlymiture.moduleyeymizeyymymyyeyeyeyymyymfileyeycymyyamlyymymrate.messagefileyimum.file.__fileymymymymymition.filey.pathy.file, file_pathificate.filefile.file_file.with__tokenymythym_file.from__file_message.from.__file_pyfromfile =pyfile_user_pathfile_projecti_file__path_file_file.user_fromfile_frompath_modelym_from_from_from_<ut_with_ut_<byte_file_from_file_tokenize_get =py_user_user_user_model_project_utils_util_r_
+class ValidationError(ValidationError, werkzeug.exceptions.Abort):
+      def __init__(self, message: str = None, *args, **kwargs):
+          super().__init__(self, message)
+          self.message = self.message
+          self.args = werkzeug.yaml.dump("{message: ", "message}:{0}")
+
+
+      def __init__message__message:
+          message: "message") -> werkzeug.message:
+          werkzeug:
+              message:werk.message:
+
+            message: {message: message:int message message:message:message: werk:message: message:message: message:message: message:message:message:message:message:message:werk>message:message:message:message:message:message:message:werk:message:werk:message: message:message:werk:message:message:werk:message:message:message:werk:message:message:message:werk:werk/message:message:message
+message:message:message:message:werk:message:message:message:message:message:message:message:message:message:message
+message:message:message:message:message:message:message:message:message:message
+message:message:message:message:message:message:message:message:message:message:message:message:message:message:message, message
+message_message:message_message.mayificate.may_message.message.message:message =file.message.maybe =maybe.file:message:message:file:file:file:message:message:message:move.message:message:message:file:file:message)maybefile_file,message,tokenfile,file, filefile(message.file_filefile_file.file_file_file_file.message_file.path.__token.user_file.tokenfilefile_from<file__file_file_file_message_file_filefile.user_file_from__withfile_from_file_userpy____env_file_from_from_input_file_user_file_email_from_fi_from_<path_file
